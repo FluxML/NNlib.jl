d8ae98b02e874cfe0ab4a4172bad92b50c2ee8a1
Mike J Innes <mike.j.innes@gmail.com>
2017-08-21 17:53:26 +0100

    activation functions

--- src/NNlib.jl
+++ src/NNlib.jl
@@ -1,5 +1,11 @@
 module NNlib
 
-# package code goes here
+export σ, relu, softmax
+
+σ(x) = 1 / (1 + exp(-x))
+
+relu(x) = max(0, x)
+
+softmax(xs) = exp.(xs) ./ sum(exp.(xs), ndims(xs))
 
 end # module
