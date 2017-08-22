3fca9d078ca72a356c4c687693c3999545e91da2
Mike J Innes <mike.j.innes@gmail.com>
2017-08-23 00:44:49 +0100

    basic softmax gradient

--- src/activation.jl
+++ src/activation.jl
@@ -6,5 +6,12 @@ softmax!(out::AbstractVecOrMat, xs::AbstractVecOrMat) =
   out .= exp.(xs) ./ sum(exp, xs, 1)
 
 softmax!(xs) = softmax!(xs, xs)
-
 softmax(xs) = softmax!(similar(xs), xs)
+
+function ∇softmax!(out::AbstractVecOrMat, Δ::AbstractVecOrMat, xs::AbstractVecOrMat)
+  s = sum(exp, xs, 1)
+  out .= exp.(xs)./s.*(Δ .- sum(Δ .* exp.(xs), 1)./s)
+end
+
+∇softmax!(Δ, xs) = ∇softmax!(Δ, Δ, xs)
+∇softmax(Δ, xs) = ∇softmax!(similar(Δ), Δ, xs)
