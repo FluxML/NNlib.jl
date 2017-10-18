8ca1eee4d1456f78b9d7b6925b04c2ce516807a7
Ariel Keselman <skariel@gmail.com>
2017-10-18 11:01:01 +0300

    added leakyrelu activation (#6)
    * added leakyrelu activation
    
    * merging relu and leakyrelu

--- src/activation.jl
+++ src/activation.jl
@@ -3,7 +3,7 @@
 # ForwardDiff numerical stability hack
 σ(x::Float32) = ifelse(x < -80, zero(x), 1 / (1 + exp(-x)))
 
-relu(x) = max(0, x)
+relu(x, leak=0.0) = max(leak.*x, x)
 
 function softmax!(out::AbstractVecOrMat, xs::AbstractVecOrMat)
   # out[end, :] .= maximum(xs, 1)
--- test/activation.jl
+++ test/activation.jl
@@ -6,4 +6,7 @@ xs = rand(5,5)
 
 @test sum(softmax(vec(xs))) ≈ 1
 
+@test relu( 0.4,0.3) ≈  0.4
+@test relu(-0.4,0.3) ≈ -0.12
+
 end
