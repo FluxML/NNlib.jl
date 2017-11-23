18ef20a2b01e4d4dcaca73c306f79081bc1071ea
Mike J Innes <mike.j.innes@gmail.com>
2017-11-23 16:10:40 +0000

    softmax tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,12 @@ using Base.Test
 @testset "NNlib" begin
 
 include("activation.jl")
-include("conv.jl")    
-    
+include("conv.jl")
+
+xs = rand(5)
+@test softmax(xs) ≈ exp.(xs) ./ sum(exp.(xs))
+
+xs = rand(5,10)
+@test softmax(xs) ≈ exp.(xs) ./ sum(exp.(xs),1)
+
 end
