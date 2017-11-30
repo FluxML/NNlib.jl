77687d57b5937a29f13f3453138aa35f59ae3e18
Mike J Innes <mike.j.innes@gmail.com>
2017-11-30 16:14:25 +0000

    use a sensible host

--- deps/build.jl
+++ deps/build.jl
@@ -3,15 +3,12 @@
 path = joinpath(dirname(@__FILE__), "..", "src")
 
 if is_apple()
-  download("https://www.dropbox.com/s/syt6q6qv6ehc4p0/nnlib-0.1.0.dylib?dl=1",
+  download("https://externalshare.blob.core.windows.net/packages/nnlib-0.1.0.dylib",
            joinpath(@__DIR__, "nnlib.dylib"))
-
 elseif is_windows()
-
   Int == Int64 || error("32-bit Windows is not currently supported. Please report this to https://github.com/FluxML/NNlib.jl")
-  download("https://www.dropbox.com/s/btxuhhpo18p9rfd/nnlib64-0.1.0.dll?dl=1",
+  download("https://externalshare.blob.core.windows.net/packages/nnlib64-0.1.0.dll",
            joinpath(@__DIR__, "nnlib.dll"))
-
 elseif is_unix()
   cd(path) do
     run(`c++ -c -fPIC -std=c++11 conv.cpp`)
