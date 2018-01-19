ec4180bae3ce2028271b863384862c178ee27594
tejank10 <tejank10@gmail.com>
2018-01-19 20:31:02 +0530

    Added logsoftmax and its tests

---
+++ src/logsoftmax.jl
@@ -0,0 +1,24 @@
+using Base.Threads
+
+function logsoftmax!(out::AbstractVecOrMat, xs::AbstractVecOrMat)
+  out .= xs .- maximum(xs, 1)
+  out .= out - log.(sum(exp.(out), 1))
+  return out
+end
+
+logsoftmax!(xs) = logsoftmax!(xs, xs)
+logsoftmax(xs) = logsoftmax!(similar(xs), xs)
+
+∇logsoftmax!(Δ, xs) = ∇softmax!(Δ, Δ ./ softmax(xs), xs)
+∇logsoftmax(Δ, xs) = ∇softmax!(similar(Δ), Δ ./ softmax(xs), xs)
+
+"""
+    logsoftmax(xs) = xs .- sum(exp.(xs))
+
+[Softmax](https://en.wikipedia.org/wiki/Softmax_function) takes
+log-probabilities (any real vector) and returns a probability distribution that
+sums to 1.
+logsoftmax computes the log of this vector. It is nunmerically more stable than
+softmax
+"""
+softmax
--- test/runtests.jl
+++ test/runtests.jl
@@ -12,4 +12,18 @@ xs = rand(5)
 xs = rand(5,10)
 @test softmax(xs) ≈ exp.(xs) ./ sum(exp.(xs),1)
 
+xs = rand(5)
+@test logsoftmax(xs) ≈ log.(softmax(xs))
+
+xs = rand(5,10)
+@test softmax(xs) ≈ log.(softmax(xs))
+
+#Comparing results from tests with TensorFlow results
+xs = Float32[-1000.,  -500., -20., 0., 20., 500., 1000.]
+@test logsoftmax(xs) ≈ [-2000., -1500., -1020., -1000., -980., -500., 0.]
+
+xs = Float32[-1000. -500. 2.; -2.   500.  1000.]
+@test logsoftmax(xs) ≈ [-1002.  -502.     0.; -1002.  -500.     0.]
+
+
 end
