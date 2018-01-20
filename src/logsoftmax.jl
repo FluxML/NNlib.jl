c9dd286a7f1de82315821d7f062a45017fb463fc
tejank10 <tejank10@gmail.com>
2018-01-20 11:55:55 +0530

    Fixed logsoftmax

--- src/logsoftmax.jl
+++ src/logsoftmax.jl
@@ -2,7 +2,7 @@ using Base.Threads
 
 function logsoftmax!(out::AbstractVecOrMat, xs::AbstractVecOrMat)
   out .= xs .- maximum(xs, 1)
-  out .= out - log.(sum(exp.(out), 1))
+  out .= out .- log.(sum(exp.(out), 1))
   return out
 end
 
@@ -13,12 +13,12 @@ logsoftmax(xs) = logsoftmax!(similar(xs), xs)
 ∇logsoftmax(Δ, xs) = ∇softmax!(similar(Δ), Δ ./ softmax(xs), xs)
 
 """
-    logsoftmax(xs) = xs .- sum(exp.(xs))
+    logsoftmax(xs) = xs .- log.(sum(exp.(xs)))
 
 [Softmax](https://en.wikipedia.org/wiki/Softmax_function) takes
 log-probabilities (any real vector) and returns a probability distribution that
 sums to 1.
-logsoftmax computes the log of this vector. It is nunmerically more stable than
+logsoftmax computes the log of this vector. It is numerically more stable than
 softmax
 """
 softmax
