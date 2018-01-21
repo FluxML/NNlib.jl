b726aeb599b309164c60c0c6a969dd0f49109bd2
tejank10 <tejank10@gmail.com>
2018-01-21 11:35:40 +0530

    Fixed logsoftmax test

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,11 +19,11 @@ xs = rand(5,10)
 @test softmax(xs) ≈ log.(softmax(xs))
 
 #Comparing results from tests with TensorFlow results
-xs = Float32[-1000.,  -500., -20., 0., 20., 500., 1000.]
+xs = Float32[-1000., -500., -20., 0., 20., 500., 1000.]
 @test logsoftmax(xs) ≈ [-2000., -1500., -1020., -1000., -980., -500., 0.]
 
-xs = Float32[-1000. -500. 2.; -2.   500.  1000.]
-@test logsoftmax(xs) ≈ [-1002.  -502.     0.; -1002.  -500.     0.]
+xs = Float32[-1000. -500. 2.; -2. 500. 1000.]
+@test logsoftmax(xs) ≈ [-998. -1000. -998.; 0. 0. 0.]
 
 
 end
