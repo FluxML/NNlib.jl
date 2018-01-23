fce7f55aa233044c459d0382c0820fb39fbcec4a
tejank10 <tejank10@gmail.com>
2018-01-23 21:06:08 +0530

    test logsoftmax

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,33 +14,10 @@ xs = rand(5,10)
 @test softmax(xs) ≈ exp.(xs) ./ sum(exp.(xs),1)
 @test logsoftmax(xs) ≈ log.(softmax(xs))
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-xs = rand(5)
-@test logsoftmax(xs) ≈ log.(softmax(xs))
-
-xs = rand(5,10)
-@test logsoftmax(xs) ≈ log.(softmax(xs))
-
-#Comparing results from tests with TensorFlow results
-xs = Float32[-1000., -500., -20., 0., 20., 500., 1000.]
-@test logsoftmax(xs) ≈ Float32[-2000., -1500., -1020., -1000., -980., -500., 0.]
-
-xs = Float32[-1000. -500. 2.; -2. 500. 1000.]
-@test logsoftmax(xs) ≈ Float32[-998. -1000. -998.; 0. 0. 0.]
-
-
-=======
-=======
->>>>>>> upstream/master
 ## compare the outputs with the PyTorch nn.LogSoftmax returns
 xs = Float32[1, 2, 3000.]
 @test logsoftmax(xs) ≈ [-2999, -2998, 0]
 
 xs = Float32[1 2 3; 1000 2000 3000]
 @test logsoftmax(xs) ≈ [-999 -1998 -2997; 0 0 0.]
-<<<<<<< HEAD
->>>>>>> upstream/master
-=======
->>>>>>> upstream/master
 end
