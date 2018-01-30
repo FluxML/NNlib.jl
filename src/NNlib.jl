92f1d424292343d56bdd7c2994bd4dc7678c102e
boathit <xiucheng90@gmail.com>
2018-01-30 20:00:36 +0800

    add logsigmoid

--- src/NNlib.jl
+++ src/NNlib.jl
@@ -2,13 +2,14 @@ module NNlib
 
 using Requires
 
-export σ, sigmoid, relu, leakyrelu, elu, swish, selu, softplus, softsign,
+export σ, sigmoid, relu, leakyrelu, elu, swish, selu, softplus, softsign, logσ, logsigmoid,
   softmax, logsoftmax, conv2d, maxpool2d, avgpool2d
 
 const libnnlib = Libdl.find_library("nnlib.$(Libdl.dlext)", [joinpath(@__DIR__, "..", "deps")])
 
 include("numeric.jl")
 include("activation.jl")
+include("logsigmoid.jl")
 include("softmax.jl")
 include("logsoftmax.jl")
 include("linalg.jl")
