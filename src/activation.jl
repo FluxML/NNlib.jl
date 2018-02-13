1e26e0aec96bcfab7449384b8213c886db98aafa
Mike J Innes <mike.j.innes@gmail.com>
2018-02-13 14:50:10 +0000

    move logsigmoid

--- src/activation.jl
+++ src/activation.jl
@@ -37,6 +37,27 @@ const sigmoid = σ
 end
 
 """
+    logσ(x)
+
+Return `log(σ(x))` which is computed in a numerically stable way.
+
+    julia> logσ(0.)
+    -0.6931471805599453
+    julia> logσ.([-100, -10, 100.])
+    3-element Array{Float64,1}:
+     -100.0
+      -10.0
+       -0.0
+"""
+function logσ(x)
+  max_v = max(zero(x), -x)
+  z = exp(-max_v) + exp(-x-max_v)
+  -(max_v + log(z))
+end
+
+const logsigmoid = logσ
+
+"""
     relu(x) = max(0, x)
 
 [Rectified Linear Unit](https://en.wikipedia.org/wiki/Rectifier_(neural_networks))
