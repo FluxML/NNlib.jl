51966bd43142abe02d13a02ecb57c048b2ea175c
americast <sayan.sinha@iitkgp.ac.in>
2018-07-11 03:22:32 +0530

    Add inplace

--- src/NNPACK.jl
+++ src/NNPACK.jl
@@ -3,7 +3,8 @@ ptp = ccall((:pthreadpool_create, :libnnpack), Ptr{Void}, (Csize_t,), 0)
 
 function softmax!(out::AbstractVecOrMat{T}, xs::AbstractVecOrMat{T}) where T<:AbstractFloat
 	input = Cfloat.(xs)
-	out = Cfloat.(out)
-	ccall((:nnp_softmax_output,"libnnpack"),Void,(Csize_t, Csize_t, Ptr{Cfloat}, Ptr{Cfloat}, Ptr{Void}), Csize_t(size(xs, 2)), Csize_t(size(xs, 1)), input, out, ptp)
-	return convert(typeof(xs), out)
+	output = Cfloat.(out)
+	ccall((:nnp_softmax_output,"libnnpack"),Void,(Csize_t, Csize_t, Ptr{Cfloat}, Ptr{Cfloat}, Ptr{Void}), Csize_t(size(xs, 2)), Csize_t(size(xs, 1)), input, output, ptp)
+	out .= output
+	return out
 end
