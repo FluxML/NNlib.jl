6fd9132c4ab1b24e3bf9aa9af726c7fe4083c6ba
pevnak <pevnak@gmail.com>
2018-11-28 06:07:33 +0100

    removed flux from tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using NNlib, Test, Flux
+using NNlib, Test
 
 @testset "NNlib" begin
 
@@ -32,11 +32,8 @@ xs = Float32[1 2 3; 1000 2000 3000]
 
 @test NNlib.∇logsoftmax(ones(size(xs)), xs) ≈ zeros(Float32, size(xs))
 @test NNlib.∇softmax(ones(size(xs)), xs) ≈ zeros(Float32, size(xs))
-gradtest(f, xs::AbstractArray...) = Flux.Tracker.gradcheck((xs...) -> sum(sin.(f(xs...))), xs...)
-@test gradtest(logsoftmax, xs)
-@test gradtest(softmax, xs)
 
-xs = randn(5, 10)
-@test gradtest(logsoftmax, xs)
-@test gradtest(softmax, xs)
+xs = [-0.238639 0.748142 -0.283194 -0.525461 -1.5348 -0.797842; 0.690384 0.211427 0.254794 -0.213572 -0.314174 -0.372663; -1.14637 -0.577988 0.718952 0.91972 -0.620773 0.929977]
+@test isapprox(NNlib.∇logsoftmax(ones(size(xs)), xs), [0.237703 -0.621474 0.448193 0.546047 0.564185 0.632273; -0.930163 0.0519798 0.0549979 0.3799 -0.477112 0.437428; 0.69246 0.569494 -0.503191 -0.925947 -0.0870738 -1.0697]; rtol = 1e-6)
+@test isapprox(NNlib.∇softmax(ones(size(xs)), xs), zeros(size(xs)); atol = 1e-6)
 end
