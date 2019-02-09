b698441cf0dfbf08875fa3364b44d95bad4ba104
Elliot Saba <staticfloat@gmail.com>
2019-02-08 21:38:10 -0500

    Major overhaul

--- src/softmax.jl
+++ src/softmax.jl
@@ -1,40 +1,5 @@
-using Base.Threads
-
-function softmax!(out::AbstractVecOrMat{T}, xs::AbstractVecOrMat{T}) where T<:AbstractFloat
-  @threads for j = 1:size(xs, 2)
-    @inbounds begin
-      # out[end, :] .= maximum(xs, 1)
-      out[end, j] = xs[end, j]
-      for i = 1:size(xs, 1)
-        out[end, j] = max(out[end, j], xs[i, j])
-      end
-      # out .= exp(xs .- out[end, :])
-      for i = 1:size(out, 1)
-        out[i, j] = exp(xs[i, j] - out[end, j])
-      end
-      # out ./= sum(out, 1)
-      s = zero(eltype(out))
-      for i = 1:size(out, 1)
-        s += out[i, j]
-      end
-      for i = 1:size(out, 1)
-        out[i, j] /= s
-      end
-    end
-  end
-  return out
-end
-
-softmax!(xs) = softmax!(xs, xs)
-softmax(xs) = softmax!(similar(xs), xs)
-
-function ∇softmax!(out::AbstractVecOrMat, Δ::AbstractVecOrMat, xs::AbstractVecOrMat)
-  sf = softmax(xs)
-  out .= sf .* (Δ .- sum(Δ .*sf, dims = 1))
-end
-
-∇softmax!(Δ, xs) = ∇softmax!(Δ, Δ, xs)
-∇softmax(Δ, xs) = ∇softmax!(similar(Δ), Δ, xs)
+export softmax, softmax!, ∇softmax, ∇softmax!,
+       logsoftmax, logsoftmax!, ∇logsoftmax, ∇logsoftmax!
 
 """
     softmax(xs) = exp.(xs) ./ sum(exp.(xs))
@@ -52,4 +17,69 @@ independent.
       0.244728
       0.665241
 """
-softmax
+softmax(xs) = softmax!(similar(xs), xs)
+
+function softmax!(out::AbstractVecOrMat{T}, xs::AbstractVecOrMat{T}) where {T}
+    @inbounds for j = 1:size(xs, 2)
+        # First, store column-wise maximum in the last element of `out`
+        out[end, j] = xs[end, j]
+        @inbounds for i = 1:(size(xs, 1) - 1)
+            out[end, j] = max(out[end, j], xs[i, j])
+        end
+
+        # Subtract the column-wise maximums to normalize, take exp()
+        # out .= exp(xs .- out[end, :])
+        @inbounds for i = 1:size(out, 1)
+            out[i, j] = exp(xs[i, j] - out[end, j])
+        end
+
+        # Normalize by sum of the entire thing
+        # out ./= sum(out, 1)
+        s = T(0)
+        @inbounds for i = 1:size(out, 1)
+            s += out[i, j]
+        end
+        @inbounds for i = 1:size(out, 1)
+            out[i, j] /= s
+        end
+    end
+    return out
+end
+
+function ∇softmax!(out::AbstractVecOrMat, Δ::AbstractVecOrMat, xs::AbstractVecOrMat)
+    sf = softmax(xs)
+    out .= sf .* (Δ .- sum(Δ .*sf, dims = 1))
+end
+
+∇softmax(Δ, xs) = ∇softmax!(similar(Δ), Δ, xs)
+∇softmax!(Δ, xs) = ∇softmax!(Δ, Δ, xs)
+
+
+"""
+    logsoftmax(xs) = log.(exp.(xs) ./ sum(exp.(xs)))
+
+`logsoftmax(xs)` computes the log of `softmax(xs)`, but in a more numerically stable
+way than directly taking the log of the the softmax function, which is commonly used in
+computing cross entropy loss.
+"""
+logsoftmax(xs) = logsoftmax!(similar(xs), xs)
+function logsoftmax!(out::AbstractVecOrMat, xs::AbstractVecOrMat)
+    for j = 1:size(xs, 2)
+        @inbounds begin
+            xi_max = xs[1, j]
+            for i = 1:size(out, 1)
+                xi_max = max(xi_max, xs[i, j])
+            end
+            s = zero(eltype(out))
+            for i = 1:size(out, 1)
+                s += exp(xs[i, j] - xi_max)
+            end
+            for i = 1:size(out, 1)
+                out[i, j] = xs[i, j] - log(s) - xi_max
+            end
+        end
+    end
+    return out
+end
+∇logsoftmax(Δ, xs) = ∇softmax(Δ ./ max.(eps(eltype(xs)),softmax(xs)), xs)
+∇logsoftmax!(Δ, xs) = ∇softmax!(Δ, Δ, xs)
