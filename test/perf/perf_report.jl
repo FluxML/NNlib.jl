f60ae1eac54bca135f149bd3407eb6aca5cea8fe
Elliot Saba <staticfloat@gmail.com>
2019-03-05 18:20:45 -0500

    Update perf testing script

--- test/perf/perf_report.jl
+++ test/perf/perf_report.jl
@@ -1,5 +1,9 @@
 using JLD2, NNlib, BenchmarkTools
 
+# We need things to go quickly here
+BenchmarkTools.DEFAULT_PARAMETERS.samples = 20
+BenchmarkTools.DEFAULT_PARAMETERS.seconds = 2.5
+
 results = Dict()
 
 function add_result(val, keys...)
@@ -14,14 +18,15 @@ function add_result(val, keys...)
     return r
 end
 
-for rank in (3, 2, 1),
-    N in (10, 20, 40, 80),
-    C_in in (1, 2, 4),
-    C_out in (1, 2, 4),
-    K in (3, 6, 12),
-    stride in (1, 2, 4),
-    dilation in (1, 2, 4),
-    padding in (0, 2, 4)
+# Modify these as needed
+for rank in (2,),
+    N in (20, 40, 80),
+    C_in in (1,),
+    C_out in (1,),
+    K in (3,),
+    stride in (1,),
+    dilation in (1,),
+    padding in (0, 2)
 
     for (conv!, ∇conv_data!, ∇conv_filter!, cT, backend) in (
             (NNlib.conv_direct!, NNlib.∇conv_data_direct!, NNlib.∇conv_filter_direct!, DenseConvDims, "direct"),
@@ -41,7 +46,12 @@ for rank in (3, 2, 1),
         catch
             continue
         end
-        y = zeros(Float32, NNlib.output_size(cdims)..., C_out, 1)
+
+        if cT == DenseConvDims
+            y = zeros(Float32, NNlib.output_size(cdims)..., C_out, 1)
+        else
+            y = zeros(Float32, NNlib.output_size(cdims)..., C_out*C_in, 1)
+        end
 
         dx = similar(x)
         dw = similar(w)
@@ -61,8 +71,9 @@ for rank in (3, 2, 1),
 end
 
 
-for rank in (3, 2, 1),
-    N in (10, 20, 40, 80),
+# Modify these as needed
+for rank in (2,),
+    N in (20,),
     K in (2, 4),
     stride in (1, 2, 4)
 
@@ -76,8 +87,8 @@ for rank in (3, 2, 1),
             (NNlib.meanpool!, NNlib.∇meanpool!, "meanpool"),
         )
 
-        t_fwd  = @benchmark pool( $y, $x, pdims)
-        t_data = @benchmark ∇pool($dx, $y, $x, pdims)
+        t_fwd  = @benchmark $(pool)( $y, $x, $pdims)
+        t_data = @benchmark $(∇pool)($dx, $y, $y, $x, $pdims)
 
         add_result(t_fwd, "$(name)$(rank)d", "direct", pdims)
         add_result(t_data, "$(name)$(rank)d_data", "direct", pdims)
