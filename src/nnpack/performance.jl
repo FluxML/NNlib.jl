94d1e007817ac5becdda541cc08bad99d91f3482
Avik Pal <avikpal@iitk.ac.in>
2019-04-30 20:50:39 +0530

    Add basic heuristics

--- src/nnpack/performance.jl
+++ src/nnpack/performance.jl
@@ -1,4 +1,16 @@
 function select_threadpool(cdims::DenseConvDims, batch_size::Int)
+    inp_size = input_size(cdims)[1] 
+    if batch_size >= 32
+        return shared_threadpool_dict[4][]
+    elseif batch_size >= 16 && inp_size >= 64
+        return shared_threadpool_dict[4][]
+    elseif inp_size <= 32
+        return C_NULL
+    elseif inp_size >= 128
+        return shared_threadpool_dict[4][]
+    elseif inp_size * batch_size >= 256
+        return shared_threadpool_dict[4][]
+    end    
     return C_NULL
 end
 
