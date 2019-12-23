5ca5eea160004d178c4a09e307cb7d8518b3ca48
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-12-23 01:31:55 -0500

    typo and ignore Manifest

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,12 @@ const is_APPVEYOR = Sys.iswindows() && haskey(ENV,"APPVEYOR")
 const is_TRAVIS = haskey(ENV,"TRAVIS")
 
 @time begin
-if GROUP == "All" || GROUP == "Core"
-    @safetestset "Activation Functions" begin include("activation.jl") end
-    @safetestset "Convolutions" begin include("conv.jl") end
-    @safetestset "Pooling Functions" begin include("pooling.jl") end
-    @safetestset "Inference" begin include("inference.jl") end
+    if GROUP == "All" || GROUP == "Core"
+        @safetestset "Activation Functions" begin include("activation.jl") end
+        @safetestset "Convolutions" begin include("conv.jl") end
+        @safetestset "Pooling Functions" begin include("pooling.jl") end
+        @safetestset "Inference" begin include("inference.jl") end
+    end
 end
 
 if GROUP == "Downstream"
