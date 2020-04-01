2709d34523c5ea284ea827091dbd57affbfc5801
Michael Abbott <me@escbook>
2020-04-02 01:28:24 +0200

    oops

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using NNlib, Test
 
-# include("activation.jl")
-# include("conv.jl")
+include("activation.jl")
+include("conv.jl")
 include("batchedmul.jl")
-# include("pooling.jl")
+include("pooling.jl")
 include("inference.jl")
