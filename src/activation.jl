3b894ba0553571c30aacadc16742e74afd71cb04
Yao Lu <luyaocns@gmail.com>
2020-05-10 14:07:45 +0800

    remove @inline

--- src/activation.jl
+++ src/activation.jl
@@ -17,7 +17,7 @@ const RealOrFloatType = Union{Real, FloatType}
 Classic [sigmoid](https://en.wikipedia.org/wiki/Sigmoid_function) activation
 function.
 """
-@inline function σ(x::RealOrFloatType)
+function σ(x::RealOrFloatType)
     t = exp(-abs(x))
     vifelse(x ≥ 0, inv(one(t) + t), t / (one(t) + t))
 end
@@ -29,7 +29,7 @@ const sigmoid = σ
 Segment-wise linear approximation of sigmoid.
 See [BinaryConnect: Training Deep Neural Networks withbinary weights during propagations](https://arxiv.org/pdf/1511.00363.pdf).
 """
-@inline hardσ(x::RealOrFloatType, a=0.2) = oftype(x/1, max(zero(x/1), min(one(x/1), oftype(x/1,a) * x + oftype(x/1,0.5))))
+hardσ(x::RealOrFloatType, a=0.2) = oftype(x/1, max(zero(x/1), min(one(x/1), oftype(x/1,a) * x + oftype(x/1,0.5))))
 const hardsigmoid = hardσ
 
 
@@ -46,7 +46,7 @@ Return `log(σ(x))` which is computed in a numerically stable way.
       -10.000045398899218
        -3.720075976020836e-44
 """
-@inline logσ(x::RealOrFloatType) = -softplus(-x)
+logσ(x::RealOrFloatType) = -softplus(-x)
 const logsigmoid = logσ
 
 
@@ -56,7 +56,7 @@ const logsigmoid = logσ
 Segment-wise linear approximation of tanh. Cheaper  and  more  computational  efficient version of tanh.
 See [Large Scale Machine Learning](http://ronan.collobert.org/pub/matos/2004_phdthesis_lip6.pdf).
 """
-@inline hardtanh(x::RealOrFloatType) = max(-one(x), min( one(x), x))
+hardtanh(x::RealOrFloatType) = max(-one(x), min( one(x), x))
 
 
 """
@@ -65,7 +65,7 @@ See [Large Scale Machine Learning](http://ronan.collobert.org/pub/matos/2004_phd
 [Rectified Linear Unit](https://en.wikipedia.org/wiki/Rectifier_(neural_networks))
 activation function.
 """
-@inline relu(x::RealOrFloatType) = max(zero(x), x)
+relu(x::RealOrFloatType) = max(zero(x), x)
 
 
 """
@@ -75,7 +75,7 @@ Leaky [Rectified Linear Unit](https://en.wikipedia.org/wiki/Rectifier_(neural_ne
 activation function.
 You can also specify the coefficient explicitly, e.g. `leakyrelu(x, 0.01)`.
 """
-@inline leakyrelu(x::RealOrFloatType, a = oftype(x / 1, 0.01)) = max(a * x, x / one(x))
+leakyrelu(x::RealOrFloatType, a = oftype(x / 1, 0.01)) = max(a * x, x / one(x))
 
 """
     relu6(x) = min(max(0, x), 6)
@@ -84,7 +84,7 @@ You can also specify the coefficient explicitly, e.g. `leakyrelu(x, 0.01)`.
 activation function capped at 6.
 See [Convolutional Deep Belief Networks on CIFAR-10](http://www.cs.utoronto.ca/%7Ekriz/conv-cifar10-aug2010.pdf)
 """
-@inline relu6(x::RealOrFloatType) = min(relu(x), oftype(x, 6))
+relu6(x::RealOrFloatType) = min(relu(x), oftype(x, 6))
 
 """
     rrelu(x, l=1/8, u=1/3) = max(a*x, x)
@@ -95,7 +95,7 @@ Randomized Leaky [Rectified Linear Unit](https://arxiv.org/pdf/1505.00853.pdf)
 activation function.
 You can also specify the bound explicitly, e.g. `rrelu(x, 0.0, 1.0)`.
 """
-@inline function rrelu(x::RealOrFloatType, l::Real = 1 / 8.0, u::Real = 1 / 3.0)
+function rrelu(x::RealOrFloatType, l::Real = 1 / 8.0, u::Real = 1 / 3.0)
     a = oftype(x / 1, (u - l) * rand() + l)
     return leakyrelu(x, a)
 end
@@ -108,7 +108,7 @@ Exponential Linear Unit activation function.
 See [Fast and Accurate Deep Network Learning by Exponential Linear Units](https://arxiv.org/abs/1511.07289).
 You can also specify the coefficient explicitly, e.g. `elu(x, 1)`.
 """
-@inline elu(x::RealOrFloatType, α = one(x)) = vifelse(x ≥ 0, x / one(x), α * (exp(x) - one(x)))
+elu(x::RealOrFloatType, α = one(x)) = vifelse(x ≥ 0, x / one(x), α * (exp(x) - one(x)))
 
 
 """
@@ -117,7 +117,7 @@ You can also specify the coefficient explicitly, e.g. `elu(x, 1)`.
 [Gaussian Error Linear Unit](https://arxiv.org/pdf/1606.08415.pdf)
 activation function.
 """
-@inline function gelu(x::RealOrFloatType)
+function gelu(x::RealOrFloatType)
     p = oftype(x / 1, π)
     λ = oftype(x / 1, √(2 / p))
     α = oftype(x / 1, 0.044715)
@@ -132,7 +132,7 @@ end
 Self-gated activation function.
 See [Swish: a Self-Gated Activation Function](https://arxiv.org/pdf/1710.05941.pdf).
 """
-@inline swish(x::RealOrFloatType) = x * σ(x)
+swish(x::RealOrFloatType) = x * σ(x)
 
 
 """
@@ -141,7 +141,7 @@ See [Swish: a Self-Gated Activation Function](https://arxiv.org/pdf/1710.05941.p
 Non-Parametric Linearly Scaled Hyperbolic Tangent Activation Function.
 See [LiSHT](https://arxiv.org/abs/1901.05894)
 """
-@inline lisht(x::RealOrFloatType) = x * tanh(x)
+lisht(x::RealOrFloatType) = x * tanh(x)
 
 
 """
@@ -153,7 +153,7 @@ See [LiSHT](https://arxiv.org/abs/1901.05894)
 Scaled exponential linear units.
 See [Self-Normalizing Neural Networks](https://arxiv.org/pdf/1706.02515.pdf).
 """
-@inline function selu(x::RealOrFloatType)
+function selu(x::RealOrFloatType)
   λ = oftype(x / 1, 1.0507009873554804934193349852946)
   α = oftype(x / 1, 1.6732632423543772848170429916717)
   λ * vifelse(x > 0, x / one(x), α * (exp(x) - one(x)))
@@ -166,7 +166,7 @@ end
 Continuously Differentiable Exponential Linear Units
 See [Continuously Differentiable Exponential Linear Units](https://arxiv.org/pdf/1704.07483.pdf).
 """
-@inline celu(x::RealOrFloatType, α::Real = one(x)) = vifelse(x ≥ 0, x / one(x), α * (exp(x/α) - one(x)))
+celu(x::RealOrFloatType, α::Real = one(x)) = vifelse(x ≥ 0, x / one(x), α * (exp(x/α) - one(x)))
 
 
 """
@@ -175,7 +175,7 @@ See [Continuously Differentiable Exponential Linear Units](https://arxiv.org/pdf
 Threshold Gated Rectified Linear.
 See [ThresholdRelu](https://arxiv.org/pdf/1402.3337.pdf)
 """
-@inline trelu(x::RealOrFloatType,theta = one(x)) = vifelse(x> theta, x, zero(x))
+trelu(x::RealOrFloatType,theta = one(x)) = vifelse(x> theta, x, zero(x))
 const thresholdrelu = trelu
 
 
@@ -184,7 +184,7 @@ const thresholdrelu = trelu
 
 See [Quadratic Polynomials Learn Better Image Features](http://www.iro.umontreal.ca/~lisa/publications2/index.php/attachments/single/205).
 """
-@inline softsign(x::RealOrFloatType) = x / (one(x) + abs(x))
+softsign(x::RealOrFloatType) = x / (one(x) + abs(x))
 
 
 """
@@ -192,7 +192,7 @@ See [Quadratic Polynomials Learn Better Image Features](http://www.iro.umontreal
 
 See [Deep Sparse Rectifier Neural Networks](http://proceedings.mlr.press/v15/glorot11a/glorot11a.pdf).
 """
-@inline softplus(x::RealOrFloatType) = vifelse(x > 0, x + log1p(exp(-x)), log1p(exp(x)))
+softplus(x::RealOrFloatType) = vifelse(x > 0, x + log1p(exp(-x)), log1p(exp(x)))
 
 
 """
@@ -200,7 +200,7 @@ See [Deep Sparse Rectifier Neural Networks](http://proceedings.mlr.press/v15/glo
 
 Return `log(cosh(x))` which is computed in a numerically stable way.
 """
-@inline logcosh(x::RealOrFloatType) = x + softplus(-2x) - log(oftype(x, 2))
+logcosh(x::RealOrFloatType) = x + softplus(-2x) - log(oftype(x, 2))
 
 
 """
@@ -209,14 +209,14 @@ Return `log(cosh(x))` which is computed in a numerically stable way.
 Self Regularized Non-Monotonic Neural Activation Function.
 See [Mish: A Self Regularized Non-Monotonic Neural Activation Function](https://arxiv.org/abs/1908.08681).
 """
-@inline mish(x::RealOrFloatType) = x * tanh(softplus(x))
+mish(x::RealOrFloatType) = x * tanh(softplus(x))
 
 """
     tanhshrink(x) = x - tanh(x)
 
 See [Tanhshrink Activation Function](https://www.gabormelli.com/RKB/Tanhshrink_Activation_Function).
 """
-@inline tanhshrink(x::RealOrFloatType) = x - tanh(x)
+tanhshrink(x::RealOrFloatType) = x - tanh(x)
 
 """
     softshrink(x, λ=0.5) =
@@ -224,7 +224,7 @@ See [Tanhshrink Activation Function](https://www.gabormelli.com/RKB/Tanhshrink_A
 
 See [Softshrink Activation Function](https://www.gabormelli.com/RKB/Softshrink_Activation_Function).
 """
-@inline softshrink(x::RealOrFloatType, λ = oftype(x/1, 0.5)) = min(max(zero(x), x - λ), x + λ)
+softshrink(x::RealOrFloatType, λ = oftype(x/1, 0.5)) = min(max(zero(x), x - λ), x + λ)
 
 # Provide an informative error message if activation functions are called with an array
 for f in (:σ, :hardσ, :logσ, :hardtanh, :relu, :leakyrelu, :relu6, :rrelu, :elu, :gelu, :swish, :lisht, :selu, :celu, :trelu, :softsign, :softplus, :logcosh, :mish, :tanhshrink, :softshrink)
