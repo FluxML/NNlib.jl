e28c86502e5945556daa4b7b8548ba49c0f5010b
Carlo Lucibello <carlo.lucibello@gmail.com>
2020-12-09 13:47:50 +0100

    cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using NNlib, Test
 
-# include("activation.jl")
-# include("conv.jl")
+include("activation.jl")
+include("conv.jl")
 include("conv_bias_act.jl")
-# include("batchedmul.jl")
-# include("pooling.jl")
-# include("inference.jl")
+include("batchedmul.jl")
+include("pooling.jl")
+include("inference.jl")
