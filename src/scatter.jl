a9ffc858992779ad55e77337bff54514ca0a08e0
Yueh-Hua Tu <a504082002@gmail.com>
2020-12-27 21:43:51 +0800

    Separate scatter mean doc

--- src/scatter.jl
+++ src/scatter.jl
@@ -10,7 +10,7 @@ Scatter operation. For each index `k` in `xs`, accumulate values in `ys` accordi
     ys[xs[k]...] = (op).(ys[xs[k]...], us[k...])
 
 # Arguments
-- `op`: operations to be applied on `ys` and `us`, e.g. `+`, `-`, `*`, `/`, `max`, `min` and `mean`.
+- `op`: operations to be applied on `ys` and `us`, e.g. `+`, `-`, `*`, `/`, `max` and `min`.
 - `ys`: the destination for `us` to aggregate to. This argument will be mutated.
 - `us`: the source data for aggregating.
 - `xs`: the mapping for aggregation from source (index) to destination (value).
@@ -31,6 +31,24 @@ function scatter!(op, ys::AbstractArray{T}, us::AbstractArray{T}, xs::AbstractAr
     ys
 end
 
+"""
+    scatter!(mean, ys, us, xs)
+
+Scatter mean operation. For each index `k` in `xs`, accumulate values in `ys` according to
+
+    ys[xs[k]...] = ys[xs[k]...] + mean.(us[k...])
+
+# Arguments
+- `ys`: the destination for `us` to aggregate to. This argument will be mutated.
+- `us`: the source data for aggregating.
+- `xs`: the mapping for aggregation from source (index) to destination (value).
+The index of `xs` is corresponding to the index of `us` and the value of `xs` is
+corresponding to the index of `ys`. The value of `xs` can be `Int` or `Tuple` type.
+
+The dimension of `us` must equal to dimension of `xs`. `ys`, `us` and `xs` must be
+supported array type and be the same type.`Array`, `StaticArray` and `CuArray`
+are currently supported.
+"""
 function scatter!(op::typeof(mean), ys::AbstractArray{T}, us::AbstractArray{T}, xs::AbstractArray{<:IntOrTuple}) where {T<:Real}
     Ns = zero(ys)
     ys_ = zero(ys)
