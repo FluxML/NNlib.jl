73a03f0346cfcee69b770e8811d317fe094ec627
Yueh-Hua Tu <a504082002@gmail.com>
2020-12-27 21:23:54 +0800

    Update src/utils.jl
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,6 @@
 """
     safe_div(x, y)
 
-Safely divde `x` by `y`. If `y` is zero, return `x` directly.
+Safely divide `x` by `y`. If `y` is zero, return `x` directly.
 """
 safe_div(x, y) = ifelse(iszero(y), x, x/y)
