1cef99ab3d33b3a81b7b99a3c5236967bded7e7c
Carlo Lucibello <carlo.lucibello@gmail.com>
2020-12-30 23:44:33 +0100

    cl/upsample

--- test/upsample.jl
+++ test/upsample.jl
@@ -1,6 +1,3 @@
-using CUDA
-CUDA.allowscalar(false)
-
 @testset "bilinear_upsample 2d" begin
     x = reshape(Float32[1. 2.; 3. 4.], (2,2,1,1))
     y_true =   [1//1  5//4    7//4    2//1;
@@ -17,14 +14,15 @@ CUDA.allowscalar(false)
 
     gradtest(x->bilinear_upsample(x, (3, 2)), x, atol=1e-4)
 
-    # CUDA compatibility
-    y = bilinear_upsample(x |> cu, (3, 2))
-    @test y isa CuArray 
-    @test Array(y) ≈ y_true
-    g_gpu = Zygote.gradient(x -> sum(sin.(bilinear_upsample(x, (3, 2))))
-                            , x |> cu)[1]
-    @test g_gpu isa CuArray
-    g_cpu = Zygote.gradient(x -> sum(sin.(bilinear_upsample(x, (3, 2))))
-                            , x)[1]
-    @test Array(g_cpu) ≈ g_cpu  atol=1e-4
+    if CUDA.has_cuda()
+        y = bilinear_upsample(x |> cu, (3, 2))
+        @test y isa CuArray 
+        @test Array(y) ≈ y_true
+        g_gpu = Zygote.gradient(x -> sum(sin.(bilinear_upsample(x, (3, 2))))
+                                , x |> cu)[1]
+        @test g_gpu isa CuArray
+        g_cpu = Zygote.gradient(x -> sum(sin.(bilinear_upsample(x, (3, 2))))
+                                , x)[1]
+        @test Array(g_cpu) ≈ g_cpu  atol=1e-4
+    end
 end
