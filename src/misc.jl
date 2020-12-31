e0541f8ea6d2d88e2fdb32be622f02738c85bf9f
Carlo Lucibello <carlo.lucibello@gmail.com>
2020-12-31 13:38:59 +0100

    fix

--- src/misc.jl
+++ src/misc.jl
@@ -3,14 +3,14 @@ export pixel_shuffle
 """
     pixel_shuffle(x, r)
     
-Pixel shuffling operation. `r` is the scale factor for shuffling.
+Pixel shuffling operation. `r` is the upscale factor for shuffling.
 The operation converts an input of size [W,H,r²C,N] to size [rW,rH,C,N]
 Used extensively in super-resolution networks to upsample 
 towards high resolution features.
 
 Reference : https://arxiv.org/pdf/1609.05158.pdf
 """
-function pixel_shuffle(x::AbstractArray{T, 4}, r::Integer) where T <:Number
+function pixel_shuffle(x::AbstractArray{T, 4}, r::Integer) where T
     w, h, c, n = size(x)
     @assert c % r^2 == 0
     c_out = c ÷ r^2
