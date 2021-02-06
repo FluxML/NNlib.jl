d5029c464947afe78d1bbc2b18e33529b6abba97
Michael Abbott <me@escbook>
2021-02-06 15:00:43 +0100

    example for upsample_bilinear

--- src/upsample.jl
+++ src/upsample.jl
@@ -106,10 +106,33 @@ can be directly specified with a keyword argument.
 The size of the output is equal to
 `(scale[1]*S1, scale[2]*S2, S3, S4)`, where `S1, S2, S3, S4 = size(x)`.
 
-Examples:
-```julia
-upsample_bilinear(x, (2, pi)) # real scaling factors are allowed
-upsample_bilinear(x; size=(64,64)) # specify ouput size
+# Examples
+
+```jldoctest
+julia> x = reshape(Float32[1 2 3; 4 5 6], (2,3,1,1))
+2×3×1×1 Array{Float32,4}:
+[:, :, 1, 1] =
+ 1.0  2.0  3.0
+ 4.0  5.0  6.0
+
+julia> upsample_bilinear(x, (2, 3))
+4×9×1×1 Array{Float32,4}:
+[:, :, 1, 1] =
+ 1.0  1.25  1.5  1.75  2.0  2.25  2.5  2.75  3.0
+ 2.0  2.25  2.5  2.75  3.0  3.25  3.5  3.75  4.0
+ 3.0  3.25  3.5  3.75  4.0  4.25  4.5  4.75  5.0
+ 4.0  4.25  4.5  4.75  5.0  5.25  5.5  5.75  6.0
+
+julia> ans == upsample_bilinear(x; size=(4, 12))  # specify ouput size instead
+
+julia> upsample_bilinear(x, (2.5, 3.5))  # non-integer scaling factors are allowed
+5×10×1×1 Array{Float32,4}:
+[:, :, 1, 1] =
+ 1.0   1.22222  1.44444  1.66667  1.88889  2.11111  2.33333  2.55556  2.77778  3.0
+ 1.75  1.97222  2.19444  2.41667  2.63889  2.86111  3.08333  3.30556  3.52778  3.75
+ 2.5   2.72222  2.94444  3.16667  3.38889  3.61111  3.83333  4.05556  4.27778  4.5
+ 3.25  3.47222  3.69444  3.91667  4.13889  4.36111  4.58333  4.80556  5.02778  5.25
+ 4.0   4.22222  4.44444  4.66667  4.88889  5.11111  5.33333  5.55556  5.77778  6.0
 ```
 """
 function upsample_bilinear(x::AbstractArray{<:Any,4}, scale::NTuple{2,Real})
