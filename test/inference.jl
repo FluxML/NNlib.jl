ffdf2af3fca3fa6a583f635672e0f116edc01009
Joost <joostve@protonmail.com>
2021-02-06 10:32:00 +0100

    Add inference tests

--- test/inference.jl
+++ test/inference.jl
@@ -1,7 +1,6 @@
-import NNlib: conv_direct, conv_im2col
+import NNlib: conv_direct, conv_im2col, channels_in, channels_out
 
 @testset "Conv Inference" begin
-    
     for T in (Float32, Float64)
         impl = [conv, conv_direct, conv_im2col]
         if NNlib.is_nnpack_available() && T == Float32 
@@ -15,4 +14,23 @@ import NNlib: conv_direct, conv_im2col
             @test @inferred(f(x, w, DenseConvDims(x, w))) isa Array{T,4} 
         end
     end
+end
+
+@testset "DenseConvDims Inference" begin
+    # this needs to be in a function to trigger inference problems
+    function channels_in_test(w::AbstractArray)
+        cdims = DenseConvDims((1,1,1,1), size(w))
+        channels_in(cdims)
+    end
+
+    # this needs to be in a function to trigger inference problems
+    function channels_out_test(w::AbstractArray)
+        cdims = DenseConvDims((1,1,1,1), size(w))
+        channels_out(cdims)
+    end
+
+    w = rand(Float32, 1, 1, 1, 1)
+
+    @test @inferred(channels_in_test(w)) isa Int
+    @test @inferred(channels_out_test(w)) isa Int
 end
