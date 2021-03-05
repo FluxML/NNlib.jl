920d7d1fa395a2d14a70909ccec81aebe4622a80
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-03-05 08:10:21 +0100

    more docs and cleanup

--- src/gather.jl
+++ src/gather.jl
@@ -3,29 +3,24 @@ export gather, gather!
 """
     gather!(dst, src, idx)
 
-Reverse operation of scatter. Gathers data from source `src` 
-and writes it in destination `dst` according to the index
-array `idx`.
+Reverse operation of [`scatter!`](@ref). Gathers data from source `src` 
+and writes it in destination `dst` according to the index array `idx`.
 For each `k` in `CartesianIndices(idx)`, assign values to `dst` according to
 
     dst[:, ... , k] .= src[:, ... , idx[k]...]
 
 Notice that if `idx` is a vector containing integers,
-and both `dst` and `src` are matrices, previous 
-expression simplifies to
+and both `dst` and `src` are matrices, previous expression simplifies to
 
     dst[:, k] .= src[:, idx[k]]
 
-and `k` will range over `1:length(idx)`. 
+and `k` will run over `1:length(idx)`. 
 
-The elements of `idx` may be repeated. A single `src` column
-can end up being copied into zero, one, or multiple `dst` columns.
+The elements of `idx` can be integers or integer tuples and may be repeated. 
+A single `src` column can end up being copied into zero, one, 
+or multiple `dst` columns.
 
-# Arguments
-
-- `dst`: the destination where data would be assigned to.
-- `src`: the source of the data to be assigned.
-- `idx`: the mapping from source to destination.
+See [`gather`](@ref) for an allocating version.
 """
 function gather!(dst::AbstractArray{Tdst,Ndst}, 
                  src::AbstractArray{Tsrc,Nsrc}, 
@@ -33,22 +28,41 @@ function gather!(dst::AbstractArray{Tdst,Ndst},
                     {Tdst, Tsrc, Ndst, Nsrc, Nidx, Tidx <: IntOrIntTuple}
 
     M = typelength(Tidx)
-    Ndst - Nidx == Nsrc - M || throw(ArgumentError("Incompatible input shapes."))
-    size(dst)[1:Ndst-Nidx] == size(src)[1:Ndst-Nidx] || throw(ArgumentError("Incompatible input shapes."))
-    size(dst)[Ndst-Nidx+1:end] == size(idx) || throw(ArgumentError("Incompatible input shapes."))
+    d = Ndst - Nidx 
+    d == Nsrc - M || throw(ArgumentError("Incompatible input shapes."))
+    size(dst)[1:d] == size(src)[1:d] || throw(ArgumentError("Incompatible input shapes."))
+    size(dst)[d+1:end] == size(idx) || throw(ArgumentError("Incompatible input shapes."))
 
-    coldst = ntuple(i -> Colon(), Ndst - Nidx)
-    colsrc = ntuple(i -> Colon(), Nsrc - M)
+    colons = ntuple(i -> Colon(), d)
     for k in CartesianIndices(idx)
-        view(dst, coldst..., k) .= view(src, colsrc..., idx[k]...)
+        view(dst, colons..., k) .= view(src, colons..., idx[k]...)
     end
     return dst
 end
 
 """
-    gather(src, idx)
+    gather(src, idx) -> dst
 
-Non-mutating version of [`gather!`](@ref).
+Reverse operation of [`scatter`](@ref). Gathers data from source `src` 
+and writes it in a destination `dst` according to the index
+array `idx`.
+For each `k` in `CartesianIndices(idx)`, assign values to `dst` 
+according to
+
+    dst[:, ... , k] .= src[:, ... , idx[k]...]
+
+Notice that if `idx` is a vector containing integers
+and `src` is a matrix, previous expression simplifies to
+
+    dst[:, k] .= src[:, idx[k]]
+
+and `k` will run over `1:length(idx)`. 
+
+The elements of `idx` can be integers or integer tuples and may be repeated. 
+A single `src` column can end up being copied into zero, one, 
+or multiple `dst` columns.
+
+See [`gather!`](@ref) for an in-place version.
 """
 function gather(src::AbstractArray{Tsrc, Nsrc}, 
                 idx::AbstractArray{Tidx, Nidx}) where 
@@ -59,6 +73,3 @@ function gather(src::AbstractArray{Tsrc, Nsrc},
     dst = similar(src, Tsrc, dstsize)
     return gather!(dst, src, idx)
 end
-
-typelength(::Type{<:Number}) = 1
-typelength(::Type{<:NTuple{M}}) where M = M
