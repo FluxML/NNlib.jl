212b531cf349a19245f73b040cd47e0cc8e60c0c
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-03-11 11:18:33 +0100

    add Manifest

--- .ci/develop_nnlibcuda.jl
+++ .ci/develop_nnlibcuda.jl
@@ -8,4 +8,8 @@ nnlibcuda = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "NNlibCUDA"))
 Pkg.develop(nnlib)
 Pkg.develop(nnlibcuda)
 
+## Do this for the time being since test doesn't pick up the manifest
+## for some reason. Can remove this and manifests when cuda 3.0 is released. 
+# Pkg.add(url="https://github.com/JuliaGPU/CUDA.jl.git", rev="master")
+
 Pkg.precompile()
--- .ci/test_nnlibcuda.jl
+++ .ci/test_nnlibcuda.jl
@@ -2,8 +2,5 @@ import Pkg
 # import NNlibCUDA
 pkgs = ["NNlibCUDA"]
 
-# Do this for the time being since test doesn't pick up the manifest
-# for some reason. Can remove this and manifests when cuda 3.0 is released. 
-Pkg.add(url="https://github.com/JuliaGPU/CUDA.jl.git", rev="master")
 
 Pkg.test(pkgs; coverage = true)
