180246dd416d588c84bb78db1d19193311e94dc9
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-03-11 12:07:03 +0100

    remove manifest

--- .ci/develop_nnlibcuda.jl
+++ .ci/develop_nnlibcuda.jl
@@ -10,6 +10,6 @@ Pkg.develop(nnlibcuda)
 
 ## Do this for the time being since test doesn't pick up the manifest
 ## for some reason. Can remove this and manifests when cuda 3.0 is released. 
-# Pkg.add(url="https://github.com/JuliaGPU/CUDA.jl.git", rev="master")
+Pkg.add(url="https://github.com/JuliaGPU/CUDA.jl.git", rev="master")
 
 Pkg.precompile()
