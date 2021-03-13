c4fe97f66dbe88425db6af92ebba3a35b59af778
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-03-13 14:15:33 +0530

    pick groups with local memory

--- src/conv.jl
+++ src/conv.jl
@@ -51,11 +51,11 @@ export conv, conv!, ∇conv_data, ∇conv_data!, ∇conv_filter, ∇conv_filter!
 Apply convolution filter `w` to input `x`. `x` and `w` are 3d/4d/5d tensors 
 in 1d/2d/3d convolutions respectively. 
 """
-function conv(x, w::AbstractArray{T, N}; stride=1, pad=0, dilation=1, flipped=false) where {T, N}
+function conv(x, w::AbstractArray{T, N}; stride=1, pad=0, dilation=1, flipped=false, groups = 1) where {T, N}
     stride = expand(Val(N-2), stride)
     pad = expand(Val(N-2), pad)
     dilation = expand(Val(N-2), dilation)
-    cdims = DenseConvDims(x, w; stride=stride, padding=pad, dilation=dilation, flipkernel=flipped)
+    cdims = DenseConvDims(x, w; stride=stride, padding=pad, dilation=dilation, flipkernel=flipped, groups = groups)
     return conv(x, w, cdims)
 end
 
@@ -86,8 +86,8 @@ for backend in (Symbol(), :_direct, :_im2col, :_nnpack)
             function $(Symbol("$(name)$(backend)"))(
                             x::AbstractArray{xT,N}, w::AbstractArray{wT,N},
                             cdims::ConvDims; kwargs...) where {xT, wT, N}
-                y = similar(x, promote_type(xT, wT), output_size(cdims)...,
-                               channels_out(cdims), size(x,N))
+                y = zero.(similar(x, promote_type(xT, wT), output_size(cdims)...,
+                               channels_out(cdims), size(x,N)))
                 return $(Symbol("$(name)$(backend)!"))(y, x, w, cdims; kwargs...)
             end
         end
@@ -145,16 +145,32 @@ for front_name in (:conv, :∇conv_data, :∇conv_filter,
                                 y::AbstractArray{yT,$N}, x::AbstractArray{xT,$N},
                                 w::AbstractArray{wT,$N}, cdims::ConvDims;
                                 kwargs...) where {yT, xT, wT}
-                    $(Symbol("$(front_name)$(backend)!"))(
-                        insert_singleton_spatial_dimension(y, $(5 - N)),
-                        insert_singleton_spatial_dimension(x, $(5 - N)),
-                        insert_singleton_spatial_dimension(w, $(5 - N)),
-                        insert_singleton_spatial_dimension(cdims, $(5 - N));
-                        kwargs...
-                    )
 
+                    y_ = insert_singleton_spatial_dimension(y, $(5 - N))
+                    x_ = insert_singleton_spatial_dimension(x, $(5 - N))
+                    w_ = insert_singleton_spatial_dimension(w, $(5 - N))
+                    cdims_ = insert_singleton_spatial_dimension(cdims, $(5 - N))
+                    x_cs = Iterators.partition(1:size(x_, $N), channels_in(cdims) ÷ groupcount(cdims))
+                    w_cs = Iterators.partition(1:size(w_, $N + 1), channels_out(cdims) ÷ groupcount(cdims))
+                    @show size(x_), size(w_), size(y_)
+                    for (xc, wc) in zip(x_cs, w_cs)
+                      yix = ntuple(i -> i == $N ? wc : Colon(), $(N + 1))
+                      wix = ntuple(i -> i == $N + 1 ? wc : Colon(), $N + 1)
+                      xix = ntuple(i -> i == $N ? xc : Colon(), $N + 1)
+                      @show xix, wix, yix
+                      y_chunk = @view y_[yix...]
+                      x_chunk = @view x_[xix...]
+                      w_chunk = @view w_[wix...]
+                      @show size(x_chunk), size(w_chunk), size(y_chunk)
+                      # $(Symbol("$(front_name)$(backend)!"))(y_chunk, x_chunk, w_chunk, cdims_; kwargs...)
+                      ynew = $(Symbol("$(front_name)$(backend)"))(x_chunk, w_chunk, cdims_; kwargs...)
+                      # @show size(ynew)
+                      return ynew
+                      y_chunk .= ynew
+                    end
                     # We explicitly return `y` here, because the backend call
                     # itself may return a reshaped view, which we don't want.
+                    # $(Symbol("$(front_name)$(backend)!"))(y_, x_, w_, cdims_; kwargs...)
                     return y
                 end
             end
