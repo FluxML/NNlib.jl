038032466480d1019175ba1cbe5fd44e1b433cfc
Shivanshu Tyagi <shivanshutyagi3@gmail.com>
2021-03-24 18:13:43 +0530

    Add support for 1d and 2d images in fold.jl

--- src/fold.jl
+++ src/fold.jl
@@ -3,66 +3,68 @@ export unfold, fold
 """
     unfold(X, W; stride=1, padding=0, dilation=1)
 Extracts sliding local blocks from a batched input tensor. X is the input 5d vector of size 
-`(image_w, image_h, image_d, channels, batch_size)`. Only 3D images are supported. Can be 
-used for 2D or 1D images, by adding an extra singleton dimention. W is the size of kernel, 
-in format `(kernel_w, kernel_h, kernel_d, channels_in, channels_out)`. Note that 
-`channels_in = channels_out`. Output has the size of `(L, channels*kernel_w*kernel_h*kernel_d, batch_size)`, 
+`(spatial_dims... , channels, batch_size)`. W is the size of kernel, in format 
+`(spatial_dims... , channels)`. Output has the size of `(L, channels*kernel_w*kernel_h*kernel_d, batch_size)`, 
 where L is the total number of blocks.
 
 """
-function unfold(X::AbstractArray{T,5} where T, w_dim::NTuple{5}; stride=1, padding=0, dilation=1)
+function unfold(X::AbstractArray{T,M} where T, w_dim::NTuple{K}; stride=1, padding=0, dilation=1) where M where K
     x_dim = size(X)
-    if w_dim[end] != w_dim[end-1]
-        throw(DimensionMismatch("input channels must be equal to output channels in the kernel"))
+    if ndims(X) > 5 || ndims(X) < 3
+        throw(DimensionMismatch("X and W must be 3d/4d/5d for 1d/2d/3d image. got $(ndims(X))d input"))
     end
+
+    if ndims(X)-2 != length(w_dim)-1
+        throw(DimensionMismatch("spatial dimentions of image and kernel must be equal, got $(ndims(X)-2),$(length(w_dim)-1)"))
+    end
+
+    # reassign x_dim after converting it to a 3d image type input
+    x_dim = ( x_dim[1:end-2]... , fill(1,5-ndims(X))... , x_dim[end-1:end]... )
+    # w_dim must be in following format: (spatial_dims..., channels_in, channels_out)
+    w_dim = ( w_dim[1:end-1]... , fill(1,4-length(w_dim))... , w_dim[end], w_dim[end] ) 
+    X = reshape(X, x_dim)
 
     # Make DenseConvDims object
     cdims = DenseConvDims(x_dim, w_dim; stride=stride, padding=padding, dilation=dilation)
 
     # Calculate the total number of sliding blocks
-    col_dim = (im2col_dims(cdims))[1:2]
-    col = undef
+    col_dim = (im2col_dims(cdims))[1:2] # im2col_dims() returns (col_dim_x, col_dim_y, thread_num)
+    col = fill(0., col_dim[1],col_dim[2],x_dim[end]) # x_dim[end] is number of batches
 
     # Iterate through all batchs
-    for i = 1:x_dim[end]
-        temp = fill(0., col_dim)
-        im2col!(temp, X[:,:,:,:,i], cdims)
-        if i == 1
-            col = reshape(temp, col_dim[1], col_dim[2], 1)
-        else
-            col = cat(dims=3, col, temp)
-        end
+    @views for i = 1:x_dim[end]
+        im2col!(col[:,:,i], X[:,:,:,:,i], cdims)
     end
-    return col;
+    return col
 end
 
 """
     fold(col, out_dim, W; stride=1, padding=0, dilation=1)
 Does the opposite of `unfold()`, Combines an array of sliding local blocks into a large containing
 tensor. `col` is a 3d array of shape `(L, channels*kernel_w*kernel_h*kernel_d, batch_size)`, where,
-L is the total number of blocks. out_dim is the spatial dimention of the required image. Note that 
-this is a 3d tuple. W is the spatial dimentions of the kernel(3d).
+L is the total number of blocks. out_dim is the spatial dimention of the required image. W is the 
+spatial dimentions of the kernel.
 
 """
-function fold(col::AbstractArray{T,3} where T, out_dim::NTuple{3}, w_dim::NTuple{3}; stride=1, padding=0, dilation=1)
+function fold(col::AbstractArray{T,3} where T, out_dim::NTuple{M}, w_dim::NTuple{M}; stride=1, padding=0, dilation=1) where M
+    # Validate input
+    if length(out_dim) > 3
+        throw(DimensionMismatch("output dimentions cannot be greater than 3, got $(ndims(out_dim))"))
+    end
+
     # Create DenseConvDims object
     col_dim = size(col)
     channels = col_dim[2]÷prod(w_dim)
-    x_dim = (out_dim[1],out_dim[2],out_dim[3],channels,col_dim[3])
-    w_dim = (w_dim[1],w_dim[2],w_dim[3],channels,channels)
+    x_dim = (out_dim... , fill(3-length(out_dim))... , channels,col_dim[3])
+    w_dim = (w_dim... , fill(3-length(w_dim))... , channels,channels)
     cdims = DenseConvDims(x_dim,w_dim; stride=stride, padding=padding, dilation=dilation)
 
-    img = undef
+    img = fill(0., x_dim)
 
     # Iterate through all batchs
-    for i = 1:x_dim[end]
-        temp = fill(0., x_dim[1:4])
-        col2im!(temp, col[:,:,i], cdims)
-        if i == 1
-            img = reshape(temp, x_dim[1], x_dim[2], x_dim[3], x_dim[4], 1)
-        else
-            img = cat(dims=5, img, temp)
-        end
+    @views for i = 1:x_dim[end]
+        col2im!(img[:,:,:,:,i], col[:,:,i], cdims)
     end
-    return img;
+
+    return reshape(img, (out_dim... , channels,col_dim[3]))
 end
