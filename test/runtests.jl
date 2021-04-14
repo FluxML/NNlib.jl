11e0da593c591a42d976bc796a95418823fe7a09
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-04-15 04:53:18 +0530

    handle if env variable not set

--- test/runtests.jl
+++ test/runtests.jl
@@ -59,7 +59,7 @@ end
 using CUDA
 
 if VERSION >= v"1.6" && CUDA.functional()
-    if ENV["NNLIB_TEST_CUDA"] == "true"
+    if get(ENV, "NNLIB_TEST_CUDA", "false") == "true"
         import Pkg
         Pkg.develop(url = "https://github.com/FluxML/NNlibCUDA.jl")
         using NNlibCUDA
