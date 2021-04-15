731c899fc6fc29bb1a183295dee1adac0fcaba42
Yueh-Hua Tu <a504082002@gmail.com>
2021-04-15 19:59:40 +0800

    support maximum_dims for CartesianIndex

--- test/utils.jl
+++ test/utils.jl
@@ -6,4 +6,8 @@
     ind3 = [(3,4,5) (1,2,3) (2,3,9);
             (4,6,2) (5,3,2) (4,4,4)]
     @test NNlib.maximum_dims(ind3) == (5,6,9)
+    ind4 = CartesianIndex.(
+           [(3,4,5) (1,2,3) (2,3,9);
+            (4,6,2) (5,3,2) (4,4,4)])
+    @test NNlib.maximum_dims(ind4) == (5,6,9)
 end
