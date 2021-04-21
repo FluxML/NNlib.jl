67a0a696eb9a6cd22c48dd3814916de9d8d8af7f
Yueh-Hua Tu <a504082002@gmail.com>
2021-04-21 23:55:38 +0800

    correct API

--- ext/NNlibCUDA/src/scatter.jl
+++ ext/NNlibCUDA/src/scatter.jl
@@ -1,34 +1,35 @@
-for op in [+, -, *, /, max, min, &, |]
-    @eval function scatter_kernel!(op::typeof($(op)), dst, src, idx)
-        index = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+# supported op: +, -, *, /, max, min, &, |, mean
 
-        @inbounds if index <= length(idx)
-            @atomic dst[idx[index]...] = $(op)(dst[idx[index]...], src[index])
-        end
-        return nothing
+function scatter_kernel!(op, dst, src, idx)
+    index = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+
+    @inbounds if index <= length(idx)
+        @atomic dst[idx[index]...] = op(dst[idx[index]...], src[index])
     end
-
-    @eval function scatter_kernel!(op::typeof($(op)), dst, src, idx, dims::Val{N}, max_idx, max_dims_idx, dims_size) where {N}
-        index = threadIdx().x + (blockIdx().x - 1) * blockDim().x
-
-        @inbounds if index <= max_idx
-            j, k = divrem(index-1, max_dims_idx)
-            dims_i = CartesianIndices(dims_size)[k+1]
-            @atomic dst[Tuple(dims_i)..., idx[j+1]...] = $(op)(dst[Tuple(dims_i)..., idx[j+1]...], src[index])
-        end
-        return nothing
-    end
+    return nothing
 end
 
-function NNlib.scatter!(op, dst::AnyCuArray{Tdst}, src::AnyCuArray{Tsrc}, idx::AnyCuArray{<:IntOrIntTuple}, dims::Val{N}) where {Tdst,Tsrc,N}
-    args = if N == 0
+function scatter_kernel!(op, dst, src, idx, max_idx, max_dims_idx, dims_size)
+    index = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+
+    @inbounds if index <= max_idx
+        j, k = divrem(index-1, max_dims_idx)
+        dims_i = CartesianIndices(dims_size)[k+1]
+        @atomic dst[Tuple(dims_i)..., idx[j+1]...] = op(dst[Tuple(dims_i)..., idx[j+1]...], src[index])
+    end
+    return nothing
+end
+
+function NNlib.scatter!(op, dst::AnyCuArray, src::AnyCuArray, idx::AnyCuArray)
+    dims = NNlib._check_dims(dst, src, idx)
+    args = if dims == 0
         max_idx = length(idx)
         op, dst, src, idx
     else
-        dims_size = size(dst)[1:N]
+        dims_size = size(dst)[1:dims]
         max_dims_idx = prod(dims_size)
         max_idx = max_dims_idx * length(idx)
-        op, dst, src, idx, dims, max_idx, max_dims_idx, dims_size
+        op, dst, src, idx, max_idx, max_dims_idx, dims_size
     end
 
     kernel = @cuda launch=false scatter_kernel!(args...)
@@ -36,5 +37,12 @@ function NNlib.scatter!(op, dst::AnyCuArray{Tdst}, src::AnyCuArray{Tsrc}, idx::A
     threads = min(max_idx, config.threads)
     blocks = cld(max_idx, threads)
     kernel(args...; threads=threads, blocks=blocks)
+    return dst
+end
+
+function NNlib.scatter!(op::typeof(mean), dst::AnyCuArray, src::AnyCuArray, idx::AnyCuArray)
+    Ns = NNlib.scatter!(+, zero(dst), one.(src), idx)
+    dst_ = NNlib.scatter!(+, zero(dst), src, idx)
+    dst .+= NNlib.safe_div.(dst_, Ns)
     return dst
 end
