96a5d3ac01894572627b21c41d6415825e7bbf9e
Anton Smirnov <anton.smn@protonmail.com>
2021-05-21 01:17:09 +0300

    Remove local stuff

--- ext/NNlibCUDA/src/NNlibCUDA.jl
+++ ext/NNlibCUDA/src/NNlibCUDA.jl
@@ -17,20 +17,4 @@ include("cudnn/softmax.jl")
 include("cudnn/activations.jl")
 include("cudnn/batchnorm.jl")
 
-function main()
-    dx = randn(Float32, 10, 10, 8, 1)
-    dw = randn(Float32, 3, 3, 1, 8)
-    dwcdims = DenseConvDims(dx, dw; groups=8)
-    @info dwcdims
-
-    dxd = dx |> cu
-    dwd = dw |> cu
-    o = NNlib.conv(dx, dw, dwcdims)
-    od = NNlib.conv(dxd, dwd, dwcdims)
-    @info "conv: ", o ≈ collect(od)
-    @info "conv filter: ", ∇conv_filter(dx, o, dwcdims) ≈ collect(∇conv_filter(dxd, od, dwcdims))
-    @info "conv data: ", ∇conv_data(o, dw, dwcdims) ≈ collect(∇conv_data(od, dwd, dwcdims))
-end
-# main()
-
 end
