136eb81667fd1494d94dd0fe8d06142f903fe2ed
Mashu <mashu@users.noreply.github.com>
2021-10-31 20:10:52 +0100

    Apply suggestions from code review
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>

--- src/functions.jl
+++ src/functions.jl
@@ -1,6 +1,6 @@
 using NNlib: sigmoid
 """
-    glu(x, dim)
+    glu(x, dim = 1)
 
 The gated linear unit.
 
@@ -9,11 +9,11 @@ Calculates `glu(x, dim) = a ⊗ σ(b)`, where
   then the element-wise product between above matrices is computed
 See ["Language Modeling with Gated Convolutional Networks"](https://arxiv.org/abs/1612.08083).
 """
-function glu(x, dim)
-    maxdim = maximum(axes(x,dim))
+function glu(x, dim = 1)
+    maxdim = size(x, dim)
     @assert maxdim % 2 == 0 "Dimension must be even"
-    half = Int(maxdim / 2)
-    a,b = selectdim(x, dim, 1:half), selectdim(x,dim, half+1:maxdim)
-    a.*sigmoid.(b)
+    half = maxdim ÷ 2
+    a, b = selectdim(x, dim, 1:half), selectdim(x, dim, half+1:maxdim)
+    a .* sigmoid.(b)
 end
 
