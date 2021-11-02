0ae4b3d59a83d11260dc616b4fdf46d094c0c0a6
Anton Smirnov <anton.smn@protonmail.com>
2021-11-02 17:15:23 +0200

    Re-use kernel functions from NNlib

--- ext/NNlibCUDA/src/sampling.jl
+++ ext/NNlibCUDA/src/sampling.jl
@@ -1,3 +1,7 @@
+@inline function NNlib._safe_add!(dx::CuDeviceArray{T, 4}, value, ix, iy, c, n) where T
+    @inbounds CUDA.@atomic dx[ix, iy, c, n] += value
+end
+
 function grid_sample_kernel!(n_elem, output, input, grid, padding_mode)
     index = (threadIdx().x - 1) + (blockIdx().x - 1) * blockDim().x
     if index < n_elem
@@ -7,29 +11,8 @@ function grid_sample_kernel!(n_elem, output, input, grid, padding_mode)
         w = index % gW + 1
         h = (index ÷ gW) % gH + 1
         n = index ÷ (gW * gH) + 1
-        # Get the corresponding (x, y) coordinates from the grid.
-        @inbounds x, y = grid[1, w, h, n], grid[2, w, h, n]
-        ix = NNlib.compute_source_index(x, iW, padding_mode)
-        iy = NNlib.compute_source_index(y, iH, padding_mode)
-        # Get corner pixel values from (ix, iy) in north-east-south-west directions.
-        ix_nw, iy_nw = floor(Int, ix), floor(Int64, iy)
-        ix_ne, iy_ne = ix_nw + 1, iy_nw
-        ix_sw, iy_sw = ix_nw, iy_nw + 1
-        ix_se, iy_se = ix_ne, iy_sw
-        # Get surfaces to each neighbor (a.k.a. interpolation weights).
-        nw = (ix_se - ix) * (iy_se - iy)
-        ne = (ix - ix_sw) * (iy_sw - iy)
-        sw = (ix_ne - ix) * (iy - iy_ne)
-        se = (ix - ix_nw) * (iy - iy_nw)
-        # ∀ channel: Calculate billinear weighted pixel value.
-        @inbounds for c in 1:iC
-            r = 0.0
-            NNlib.in_bounds(iy_nw, ix_nw, iH, iW) && (r += input[ix_nw, iy_nw, c, n] * nw)
-            NNlib.in_bounds(iy_ne, ix_ne, iH, iW) && (r += input[ix_ne, iy_ne, c, n] * ne)
-            NNlib.in_bounds(iy_sw, ix_sw, iH, iW) && (r += input[ix_sw, iy_sw, c, n] * sw)
-            NNlib.in_bounds(iy_se, ix_se, iH, iW) && (r += input[ix_se, iy_se, c, n] * se)
-            output[w, h, c, n] = r
-        end
+        NNlib._grid_sample_kernel!(
+            output, input, grid, padding_mode, w, h, n, iW, iH, iC)
     end
     nothing
 end
@@ -43,53 +26,8 @@ function ∇grid_sample_kernel!(n_elem, dx, dgrid, Δ, input, grid, padding_mode
         w = index % gW + 1
         h = (index ÷ gW) % gH + 1
         n = index ÷ (gW * gH) + 1
-        # Get corresponding (x, y) from grid.
-        @inbounds x, y = grid[1, w, h, n], grid[2, w, h, n]
-        # Compute multipliers for gradinets on ix, iy.
-        ix, gix_mult = NNlib.∇compute_source_index(x, iW, padding_mode)
-        iy, giy_mult = NNlib.∇compute_source_index(y, iH, padding_mode)
-        # Get corner pixel values from (ix, iy) in north-east-south-west directions.
-        ix_nw, iy_nw = floor(Int, ix), floor(Int64, iy)
-        ix_ne, iy_ne = ix_nw + 1, iy_nw
-        ix_sw, iy_sw = ix_nw, iy_nw + 1
-        ix_se, iy_se = ix_ne, iy_sw
-        # Get surfaces to each neighbor (a.k.a. interpolation weights).
-        nw = (ix_se - ix) * (iy_se - iy)
-        ne = (ix - ix_sw) * (iy_sw - iy)
-        sw = (ix_ne - ix) * (iy - iy_ne)
-        se = (ix - ix_nw) * (iy - iy_nw)
-        # ∀ channel: Calculate billinear weighted pixel value.
-        gix, giy = 0.0, 0.0
-        @inbounds for c in 1:iC
-            g_out = Δ[w, h, c, n]
-            # Calculate dx and dgrid partials.
-            if NNlib.in_bounds(iy_nw, ix_nw, iH, iW)
-                CUDA.@atomic dx[ix_nw, iy_nw, c, n] += g_out * nw
-                nw_val = input[ix_nw, iy_nw, c, n]
-                gix -= nw_val * (iy_se - iy) * g_out
-                giy -= nw_val * (ix_se - ix) * g_out
-            end
-            if NNlib.in_bounds(iy_ne, ix_ne, iH, iW)
-                CUDA.@atomic dx[ix_ne, iy_ne, c, n] += g_out * ne
-                ne_val = input[ix_ne, iy_ne, c, n]
-                gix += ne_val * (iy_sw - iy) * g_out
-                giy -= ne_val * (ix - ix_sw) * g_out
-            end
-            if NNlib.in_bounds(iy_sw, ix_sw, iH, iW)
-                CUDA.@atomic dx[ix_sw, iy_sw, c, n] += g_out * sw
-                sw_val = input[ix_sw, iy_sw, c, n]
-                gix -= sw_val * (iy - iy_ne) * g_out
-                giy += sw_val * (ix_ne - ix) * g_out
-            end
-            if NNlib.in_bounds(iy_se, ix_se, iH, iW)
-                CUDA.@atomic dx[ix_se, iy_se, c, n] += g_out * se
-                se_val = input[ix_se, iy_se, c, n]
-                gix += se_val * (iy - iy_nw) * g_out
-                giy += se_val * (ix - ix_nw) * g_out
-            end
-        end
-        @inbounds dgrid[1, w, h, n] = gix_mult * gix
-        @inbounds dgrid[2,w, h, n] = giy_mult * giy
+        NNlib._∇grid_sample_kernel!(
+            dx, dgrid, Δ, input, grid, padding_mode, w, h, n, iW, iH, iC)
     end
     nothing
 end
