a9f9d7f78d4e5f65094ddf2dd1bacc53a47b145d
tylerjthomas9 <tylerjthomas9@gmail.com>
2021-11-22 12:26:06 -0600

    update doctest, gradient, AbstractVector support

--- src/sparsemax.jl
+++ src/sparsemax.jl
@@ -11,24 +11,52 @@ by default (`dims = 1`) treat it as a batch of vectors, with each column indepen
 # Examples
 
 ```jldoctest
-julia> sparsemax([1 2 3; 2 2 2])  # dims=1
-2×3 Matrix{Float64}:
-0.0  0.5  1.0
-1.0  0.5  0.0
+julia> sparsemax(reshape(1:15, 5,3) ./ 2) # dims=1
+5×3 Matrix{Float64}:
+ 0.0   0.0   0.0
+ 0.0   0.0   0.0
+ 0.0   0.0   0.0
+ 0.25  0.25  0.25
+ 0.75  0.75  0.75
 
-julia> sparsemax([1 2 3; 2 2 2]; dims=2)
-2×3 Matrix{Float64}:
-0.0       0.0       1.0
-0.333333  0.333333  0.333333
+julia> sparsemax(reshape(1:15, 5, 3) ./ 20) # Scale matters
+5×3 Matrix{Float64}:
+ 0.1   0.1   0.1
+ 0.15  0.15  0.15
+ 0.2   0.2   0.2
+ 0.25  0.25  0.25
+ 0.3   0.3   0.3
+
+ julia> sparsemax(reshape(1:15, 5, 3) ./ 20 .+ [-20 0 20])
+ 5×3 Matrix{Float64}:
+  0.1   0.1   0.1
+  0.15  0.15  0.15
+  0.2   0.2   0.2
+  0.25  0.25  0.25
+  0.3   0.3   0.3
 ```
 """
 function sparsemax(x::AbstractArray; dims::Integer=1)
-    z = slicemap(x -> reverse(sort(x)), x; dims=dims)  # float is usually free, except on integers etc.
+    z = sort(float(x); dims, rev=true)   # float is usually free, except on integers etc.
     mask = _sparsemax_mask(z, dims)
     tausum = sum(z .* mask; dims)  # no longer need z
     kay = sum(mask; dims)
     z = _relu.(x  .- (tausum .- 1) ./ kay)
 end
+
+function sparsemax(x::AbstractVector; dims::Integer=1)
+    if dims == 1
+        z = sort(float(x); rev=true)
+    elseif dims == 2
+        z = float(x)
+    end
+    mask = _sparsemax_mask(z, dims)
+    tausum = sum(z .* mask; dims)  # no longer need z
+    kay = sum(mask; dims)
+    z = _relu.(x  .- (tausum .- 1) ./ kay)
+end
+
+
 
 function _sparsemax_mask(z::AbstractArray, dim::Integer)
     acc = cumsum(z; dims=dim)
@@ -48,14 +76,15 @@ _relu(x) = _ifelse(x>0, x, false)  # different gradient at zero
 
 _ifelse(p, x, y) = ifelse(p, promote(x, y)...)
 
-function ∇sparsemax(dy::AbstractArray, y::AbstractArray; dims::Integer=1)
-    vee = sum(dy .* (y .> 0); dims)
-    kay = count(>(0), y; dims)  # could also keep from forward pass?
-    _ifelse.(y .> 0, dy .- vee ./ kay, 0)
-end
+function ∇sparsemax(Δ::AbstractArray, y::AbstractArray; dims = 1)
+    nonzeros = Δ.!=0.0
+    out = Δ .* y
+    total = sum(out .* nonzeros; dims=dims) / sum(nonzeros; dims=dims)
+    out = nonzeros .* (out .- total)
+ end
 
 function rrule(::typeof(sparsemax), xs; dims=1)
     y = sparsemax(xs; dims=dims)
-    sparsemax_pullback(Δ) = (NoTangent(), ∇sparsemax(xs, y, dims = dims))
+    sparsemax_pullback(Δ) = (NoTangent(), ∇sparsemax(unthunk(Δ), y; dims = dims))
     return y, sparsemax_pullback
  end
