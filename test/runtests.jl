2129652cf2eea8163205e4e3b827ad2c7678fa8f
Anton Smirnov <anton.smn@protonmail.com>
2022-01-10 13:27:35 +0200

    Uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,69 +21,69 @@ const rng = StableRNG(123)
 
 include("test_utils.jl")
 
-# @testset "Activation Functions" begin
-#     include("activations.jl")
-# end
+@testset "Activation Functions" begin
+    include("activations.jl")
+end
 
-# @testset "Batched Multiplication" begin
-#     include("batchedmul.jl")
-# end
+@testset "Batched Multiplication" begin
+    include("batchedmul.jl")
+end
 
-# @testset "Convolution" begin
-#     include("conv.jl")
-#     include("conv_bias_act.jl")
-# end
+@testset "Convolution" begin
+    include("conv.jl")
+    include("conv_bias_act.jl")
+end
 
-# @testset "Inference" begin
-#     include("inference.jl")
-# end
+@testset "Inference" begin
+    include("inference.jl")
+end
 
 @testset "Pooling" begin
     include("pooling.jl")
 end
 
-# @testset "Padding" begin
-#     include("padding.jl")
-# end
+@testset "Padding" begin
+    include("padding.jl")
+end
 
-# @testset "Softmax" begin
-#     include("softmax.jl")
-# end
+@testset "Softmax" begin
+    include("softmax.jl")
+end
 
-# @testset "Upsampling" begin
-#     include("upsample.jl")
-# end
+@testset "Upsampling" begin
+    include("upsample.jl")
+end
 
-# @testset "Gather" begin
-#     include("gather.jl")
-# end
+@testset "Gather" begin
+    include("gather.jl")
+end
 
-# @testset "Scatter" begin
-#     include("scatter.jl")
-# end
+@testset "Scatter" begin
+    include("scatter.jl")
+end
 
-# @testset "Utilities" begin
-#     include("utils.jl")
-# end
+@testset "Utilities" begin
+    include("utils.jl")
+end
 
-# @testset "Grid Sampling" begin
-#     include("sampling.jl")
-# end
+@testset "Grid Sampling" begin
+    include("sampling.jl")
+end
 
-# @testset "Functions" begin
-#     include("functions.jl")
-# end
+@testset "Functions" begin
+    include("functions.jl")
+end
 
-# if VERSION >= v"1.6" && CUDA.functional()
-#     if get(ENV, "NNLIB_TEST_CUDA", "false") == "true"
-#         import Pkg
-#         using NNlibCUDA
-#         @testset "CUDA" begin
-#             Pkg.test("NNlibCUDA")
-#         end
-#     else
-#         @info "Skipping CUDA tests, set NNLIB_TEST_CUDA=true to run them"
-#     end
-# else
-#     @info "Insufficient version or CUDA not found; Skipping CUDA tests"
-# end
+if VERSION >= v"1.6" && CUDA.functional()
+    if get(ENV, "NNLIB_TEST_CUDA", "false") == "true"
+        import Pkg
+        using NNlibCUDA
+        @testset "CUDA" begin
+            Pkg.test("NNlibCUDA")
+        end
+    else
+        @info "Skipping CUDA tests, set NNLIB_TEST_CUDA=true to run them"
+    end
+else
+    @info "Insufficient version or CUDA not found; Skipping CUDA tests"
+end
