e182a67fa8cb54a10c1b9a2924b8c6078235fc86
Nikola Janjusevic <nikopjj@gmail.com>
2022-11-23 11:08:58 -0500

    fold kernel flipping

--- src/fold.jl
+++ src/fold.jl
@@ -1,21 +1,22 @@
 
 """
-    unfold(x, kernel_size; stride = 1, pad = 0, dilation = 0)
+    unfold(x, kernel_size; stride = 1, pad = 0, dilation = 0, flipped = false)
 
 Places sliding windows of x into a container tensor of size (num_windows, window_size, batchsize).
 The window size is determined by the prod(spatial dims of kernel)*input_channels.
 The number of sliding windows will match those of convolution (conv) with the same kernel_size and arguments.
+Uses NNlib.im2col! as backend.
 """
-function unfold(x::AbstractArray{T, N}, kernel_size::NTuple{K}; stride = 1, pad = 0, dilation = 1) where {T, K, N}
+function unfold(x::AbstractArray{T, N}, kernel_size::NTuple{K}; stride = 1, pad = 0, dilation = 1, flipped = false) where {T, K, N}
     stride = expand(Val(N - 2), stride)
     padding = expand(Val(N - 2), pad)
     dilation = expand(Val(N - 2), dilation)
-    cdims = DenseConvDims(size(x), kernel_size; stride, padding, dilation)
+    cdims = DenseConvDims(size(x), kernel_size; stride, padding, dilation, flipkernel=flipped)
     return unfold(x, cdims)
 end
 
 """
-    fold(y, output_size, kernel_size; stride = 1, pad = 0, dilation = 0)
+    fold(y, output_size, kernel_size; stride = 1, pad = 0, dilation = 0, flipped = false)
 
 Accumulates sliding windows from the output of unfold into a container tensor of size `output_size`.
 An inverse to `unfold` can be obtained by using `fold` and accounting for scaling issues. 
@@ -48,12 +49,13 @@ julia> d = fold(unfold(ones(eltype(x), size(x)...), kernel_size; pad=pad), size(
 julia> x == z./d
 true
 ```
+Uses NNlib.col2im! as backend.
 """
-function fold(x::AbstractArray{T, 3}, output_size::NTuple{N}, kernel_size::NTuple{K}; stride = 1, pad = 0, dilation = 1) where {T, K, N}
+function fold(x::AbstractArray{T, 3}, output_size::NTuple{N}, kernel_size::NTuple{K}; stride = 1, pad = 0, dilation = 1, flipped = false) where {T, K, N}
     stride = expand(Val(N - 2), stride)
     padding = expand(Val(N - 2), pad)
     dilation = expand(Val(N - 2), dilation)
-    cdims = DenseConvDims(output_size, kernel_size; stride, padding, dilation)
+    cdims = DenseConvDims(output_size, kernel_size; stride, padding, dilation, flipkernel=flipped)
     return fold(x, output_size, cdims)
 end
 
@@ -109,26 +111,26 @@ function fold!(x::AbstractArray{xT, 5}, y::AbstractArray{yT, 3}, cdims::DenseCon
 end
 
 # reverse diff rules
-function rrule(::typeof(unfold), x, cdims)
+function rrule(::typeof(unfold), x, cdims::DenseConvDims; kw...)
     function unfold_pullback(Δ)
         return (
             NoTangent(),
-            @thunk(fold(unthunk(Δ), size(x), cdims)),
+            fold(unthunk(Δ), size(x), cdims; kw...),
             NoTangent(),
         )
     end
-    return unfold(x, cdims), unfold_pullback
+    return unfold(x, cdims; kw...), unfold_pullback
 end
 
-function rrule(::typeof(fold), x, output_size, cdims)
+function rrule(::typeof(fold), x, output_size, cdims::DenseConvDims; kw...)
     function fold_pullback(Δ)
         return (
             NoTangent(),
-            @thunk(unfold(unthunk(Δ), cdims)),
+            unfold(unthunk(Δ), cdims; kw...),
             NoTangent(),
             NoTangent(),
         )
     end
-    return fold(x, output_size, cdims), fold_pullback
+    return fold(x, output_size, cdims; kw...), fold_pullback
 end
 
