e35dffc2bcf0bced88e8fb4e88a6afe8f97bd12a
Nikola Janjušević <nikopjj@gmail.com>
2022-12-03 20:03:17 -0500

    Update src/fold.jl for 1.6 compat
    Co-authored-by: Brian Chen <ToucheSir@users.noreply.github.com>

--- ext/NNlibCUDA/src/fold.jl
+++ ext/NNlibCUDA/src/fold.jl
@@ -14,7 +14,7 @@ function unfold_kernel!(col::AbstractArray{T}, x, col_size, input_size, output_s
         end
 
         # check out of bounds
-        if any((w, h, d) .<= 0 .|| (w, h, d) .> input_size)
+        if any((w, h, d) .<= 0 .| (w, h, d) .> input_size)
             col[i, kw, kh, kd, c, b] = T(0)
             return nothing
         end
