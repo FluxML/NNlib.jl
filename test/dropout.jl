70a03c6fdab842cbbd5737f1cd54ff8c178b6540
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2023-01-04 00:48:57 -0500

    fix 1.6

--- test/dropout.jl
+++ test/dropout.jl
@@ -15,8 +15,10 @@ using Zygote, StableRNGs, ChainRulesCore
     @test size(@inferred dropout(rng, x1, 0.1)) == (3, 4)
     @test size(@inferred dropout(rng, x1, 0.1; dims=2)) == (3, 4)
 
-    x2 = Diagonal(randn(Float32, 10))
-    @test dropout(x2, 0.3) isa Matrix{Float32}  # does not infer, but that's OK?
+    x2 = Diagonal(randn(Float32, 10))  # Just to check it runs on weird matrices.
+    if VERSION > v"1.8-"  # on 1.6 this makes a sparse array.
+        @test dropout(x2, 0.3) isa Matrix{Float32}  # does not infer, but that's OK?
+    end
 
     # Values
     @test dropout(x1, 0) == x1
