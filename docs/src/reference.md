f4f980849eb423040b5af8aaec64b83671de54f5
Anton Smirnov <tonysmn97@gmail.com>
2023-02-17 14:35:07 +0200

    Flip kernel automatically

--- docs/src/reference.md
+++ docs/src/reference.md
@@ -76,9 +76,11 @@ pad_zeros
 
 `Flux`'s `Conv` and `CrossCor` layers use `NNlib.DenseConvDims` and `NNlib.conv` internally. 
 
-!!! AMDGPU MIOpen only supports cross-correlation as its convolution algorithm (`flipkernel=true`).
-    Flip convolution kernel manually before passing it to `NNlib.conv`
-    to perform regular convolution (`flipkernel=false`): `reverse(w; dims=(1, 2))`.
+!!! AMDGPU MIOpen supports only cross-correlation (flipkernel=true).
+    Therefore for every regular convolution (flipkernel=false)
+    kernel is flipped before calculation.
+    For better performance, use cross-correlation (flipkernel=true)
+    and manually flip the kernel before `NNlib.conv` call.
     `Flux` handles this automatically, this is only required for direct calls.
 
 ```@docs
--- test/amd/conv.jl
+++ test/amd/conv.jl
@@ -3,18 +3,12 @@
     for T in (Float16, Float32), nd in (1, 2, 3)
         x = rand(Float32, fill(4, nd)..., 3, 1)
         w = rand(Float32, fill(2, nd)..., channels, 4)
+
         cdims = DenseConvDims(x, w, flipkernel=true)
         gputest((x, w) -> NNlib.conv(x, w, cdims), x, w; atol=1e-4)
+
+        # This one flips manually kernel for AMDGPU.
+        cdims = DenseConvDims(x, w)
+        gputest((x, w) -> NNlib.conv(x, w, cdims), x, w; atol=1e-4)
     end
-end
-
-@testset "Regular convolution with flipped kernel" begin
-    x = rand(Float32, 16, 16, 3, 1)
-    w = rand(Float32, 2, 2, 3, 4)
-    xd, wd = ROCArray.((x, reverse(w; dims=(1, 2))))
-
-    cdims = DenseConvDims(x, w)
-    y = NNlib.conv(x, w, cdims)
-    yd = NNlib.conv(xd, wd, cdims)
-    @test Array(yd) ≈ y atol=1f-3
 end
