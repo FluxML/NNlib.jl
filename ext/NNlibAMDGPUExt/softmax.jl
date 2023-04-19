5b419939fe51870a681d408de940837606175dda
Anton Smirnov <tonysmn97@gmail.com>
2023-04-19 13:28:02 +0300

    Cast FP16 -> FP32 in softmax

--- ext/NNlibAMDGPUExt/softmax.jl
+++ ext/NNlibAMDGPUExt/softmax.jl
@@ -1,6 +1,9 @@
 for fname in (:softmax, :logsoftmax)
-    @eval function NNlib.$(fname)(x::ROCArray{T}; dims = 1) where T <: MIOPENFloat
+    @eval function NNlib.$(fname)(x::ROCArray{Float32}; dims = 1)
         MIOpen.$(fname)(x; dims)
+    end
+    @eval function NNlib.$(fname)(x::ROCArray{Float16}; dims = 1)
+        Float16.(MIOpen.$(fname)(Float32.(x); dims))
     end
 
     @eval function NNlib.$(Symbol("∇$(fname)"))(
