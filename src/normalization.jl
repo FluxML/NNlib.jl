f5ed85b49b0cf17692719e4bdf07bef9a75b1ef7
Carlo Lucibello <carlo.lucibello@gmail.com>
2023-06-16 07:35:42 +0200

    port batchnorm rrule from Flux (#499)

--- src/normalization.jl
+++ src/normalization.jl
@@ -2,3 +2,13 @@
 function batchnorm end
 
 function ∇batchnorm end
+
+
+function ChainRulesCore.rrule(::typeof(batchnorm), g, b, x, running_mean, running_var, momentum; kw...)
+  y = batchnorm(g, b, x, running_mean, running_var, momentum; kw...) 
+  function batchnorm_pullback(Δ)
+    grad = ∇batchnorm(g, b, x, unthunk(Δ), running_mean, running_var, momentum; kw...)
+    (NoTangent(), grad..., NoTangent(), NoTangent(), NoTangent())
+  end
+  y, batchnorm_pullback
+end
