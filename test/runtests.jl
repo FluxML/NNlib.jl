83df6426d5a669754399bc4a9d8920c4b52e1a76
Carlo Lucibello <carlo.lucibello@gmail.com>
2023-09-11 17:37:18 +0100

    Simplify runtests.jl (#531)
    * cl/test
    
    * reinstate cpu tests on CUDA worker

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,8 +13,9 @@ import ReverseDiff as RD        # used in `pooling.jl`
 
 DocMeta.setdocmeta!(NNlib, :DocTestSetup, :(using NNlib, UnicodePlots); recursive=true)
 
-ENV["NNLIB_TEST_CUDA"] = true # uncomment to run CUDA tests
-# ENV["NNLIB_TEST_AMDGPU"] = true # uncomment to run AMDGPU tests
+# ENV["NNLIB_TEST_CUDA"] = "true" # uncomment to run CUDA tests
+# ENV["NNLIB_TEST_AMDGPU"] = "true" # uncomment to run AMDGPU tests
+# ENV["NNLIB_TEST_CPU"] = "false" # uncomment to skip CPU tests
 
 const rng = StableRNG(123)
 include("test_utils.jl")
@@ -49,138 +50,109 @@ function nnlib_testsuite(Backend; skip_tests = Set{String}())
     end
 end
 
-@testset "NNlib.jl" verbose=true begin
-    @testset verbose=true "Test Suite" begin
-        @testset "CPU" begin
+@testset verbose=true "NNlib.jl" begin
+
+    if get(ENV, "NNLIB_TEST_CPU", "true") == "true"
+        @testset "CPU" begin      
+            @testset "Doctests" begin
+                doctest(NNlib, manual=false)
+            end
+
             nnlib_testsuite(CPU)
-        end
 
-        if get(ENV, "NNLIB_TEST_CUDA", "false") == "true"
-            using CUDA
-            if CUDA.functional()
-                @testset "CUDABackend" begin
-                    nnlib_testsuite(CUDABackend; skip_tests=Set(("Scatter", "Gather")))
-                end
-            else
-                @info "CUDA.jl is not functional. Skipping test suite for CUDABackend."
+            @testset "Activation Functions" begin
+                include("activations.jl")
+                include("bias_act.jl")
             end
-        else
-            @info "Skipping CUDA tests, set NNLIB_TEST_CUDA=true to run them."
-        end
 
-        if get(ENV, "NNLIB_TEST_AMDGPU", "false") == "true"
-            import Pkg
-            test_info = Pkg.project()
-            Pkg.develop("AMDGPU")
-
-            using AMDGPU
-            if AMDGPU.functional()
-                @testset "ROCBackend" begin
-                    nnlib_testsuite(ROCBackend)
-                    AMDGPU.synchronize(; blocking=false)
-                end
-            else
-                @info "AMDGPU.jl is not functional. Skipping test suite for ROCBackend."
+            @testset "Attention" begin
+                include("attention.jl")
             end
-        else
-            @info "Skipping AMDGPU tests, set NNLIB_TEST_AMDGPU=true to run them."
+
+            @testset "Batched Multiplication" begin
+                include("batchedmul.jl")
+            end
+
+            @testset "Convolution" begin
+                include("conv.jl")
+                include("conv_bias_act.jl")
+            end
+
+            @testset "CTC Loss" begin
+                include("ctc.jl")
+            end
+
+            @testset "Dropout" begin
+                include("dropout.jl")
+            end
+
+            @testset "Fold/Unfold" begin
+                include("fold.jl")
+            end
+
+            @testset "Inference" begin
+                include("inference.jl")
+            end
+
+            @testset "Pooling" begin
+                include("pooling.jl")
+            end
+
+            @testset "Padding" begin
+                include("padding.jl")
+            end
+
+            @testset "Softmax" begin
+                include("softmax.jl")
+            end
+
+            @testset "Utilities" begin
+                include("utils.jl")
+            end
+
+            @testset "Grid Sampling" begin
+                include("sampling.jl")
+            end
+
+            @testset "Functions" begin
+                include("functions.jl")
+            end
         end
+    else
+        @info "Skipping CPU tests, set NNLIB_TEST_CPU=true to run them."
     end
 
-    @testset verbose=true "Tests" begin
-        if get(ENV, "NNLIB_TEST_CUDA", "false") == "true"
-            using CUDA
-            if CUDA.functional()
-                @testset "CUDA" begin
-                    include("ext_cuda/runtests.jl")
-                end
-            else
-                @info "Insufficient version or CUDA not found; Skipping CUDA tests"
+    if get(ENV, "NNLIB_TEST_CUDA", "false") == "true"
+        using CUDA
+        if CUDA.functional()
+            @testset "CUDA" begin
+                nnlib_testsuite(CUDABackend; skip_tests=Set(("Scatter", "Gather")))
+
+                include("ext_cuda/runtests.jl")
             end
         else
-            @info "Skipping CUDA tests, set NNLIB_TEST_CUDA=true to run them"
+            @info "Insufficient version or CUDA not found; Skipping CUDA tests"
         end
+    else
+        @info "Skipping CUDA tests, set NNLIB_TEST_CUDA=true to run them"
+    end 
 
-        if get(ENV, "NNLIB_TEST_AMDGPU", "false") == "true"
-            import Pkg
-            test_info = Pkg.project()
-            Pkg.develop("AMDGPU")
+    if get(ENV, "NNLIB_TEST_AMDGPU", "false") == "true"
+        using AMDGPU
+        AMDGPU.versioninfo()
+        if AMDGPU.functional() && AMDGPU.functional(:MIOpen)
+            @show AMDGPU.MIOpen.version()
+            @testset "AMDGPU" begin
+                nnlib_testsuite(ROCBackend)
+                AMDGPU.synchronize(; blocking=false)
 
-            using AMDGPU
-            AMDGPU.versioninfo()
-            if AMDGPU.functional() && AMDGPU.functional(:MIOpen)
-                @show AMDGPU.MIOpen.version()
-                @testset "AMDGPU" begin
-                    include("ext_amdgpu/runtests.jl")
-                    AMDGPU.synchronize(; blocking=false)
-                end
-            else
-                @info "AMDGPU.jl package is not functional. Skipping AMDGPU tests."
+                include("ext_amdgpu/runtests.jl")
+                AMDGPU.synchronize(; blocking=false)
             end
         else
-            @info "Skipping AMDGPU tests, set NNLIB_TEST_AMDGPU=true to run them."
+            @info "AMDGPU.jl package is not functional. Skipping AMDGPU tests."
         end
-
-        @testset "Doctests" begin
-            doctest(NNlib, manual=false)
-        end
-
-        @testset "Activation Functions" begin
-            include("activations.jl")
-            include("bias_act.jl")
-        end
-
-        @testset "Attention" begin
-            include("attention.jl")
-        end
-
-        @testset "Batched Multiplication" begin
-            include("batchedmul.jl")
-        end
-
-        @testset "Convolution" begin
-            include("conv.jl")
-            include("conv_bias_act.jl")
-        end
-
-        @testset "CTC Loss" begin
-            include("ctc.jl")
-        end
-
-        @testset "Dropout" begin
-            include("dropout.jl")
-        end
-
-        @testset "Fold/Unfold" begin
-            include("fold.jl")
-        end
-
-        @testset "Inference" begin
-            include("inference.jl")
-        end
-
-        @testset "Pooling" begin
-            include("pooling.jl")
-        end
-
-        @testset "Padding" begin
-            include("padding.jl")
-        end
-
-        @testset "Softmax" begin
-            include("softmax.jl")
-        end
-
-        @testset "Utilities" begin
-            include("utils.jl")
-        end
-
-        @testset "Grid Sampling" begin
-            include("sampling.jl")
-        end
-
-        @testset "Functions" begin
-            include("functions.jl")
-        end
+    else
+        @info "Skipping AMDGPU tests, set NNLIB_TEST_AMDGPU=true to run them."
     end
 end
