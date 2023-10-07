d87f909e93239f83a570a846a2e2ba4340216392
Jonas S <74866941+jonas208@users.noreply.github.com>
2023-10-07 23:08:58 +0200

    Add files via upload

--- ext/NNlibLoopVectorizationExt/conv.jl
+++ ext/NNlibLoopVectorizationExt/conv.jl
@@ -1,5 +1,9 @@
-# # pad naivly, static iters
+# pad naivly, static iters
 function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, cdims::ConvDims) where {T<:Real}
+
+    # if cdims.stride != (1, 1)
+        # return conv_im2col_grouped!(output, input, weight, cdims)
+    # end
 
     # fix for groupcount > 1 (NNlib.check_dims would throw an error otherwise)
     size_weight_check_dims = (size(weight)[1:2]..., size(weight)[3]*cdims.groupcount, size(weight)[4])
@@ -32,6 +36,7 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
             for out_channel in 1:out_channels, y_out in 1:output_height, x_out in 1:output_width
                 value = zero(T)
                 for in_channel in static(1):static(in_channels), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
+                # for in_channel in 1:in_channels, y_w in 1:weight_height, x_w in 1:weight_width
                     value += input[x_out + x_w - 1, y_out + y_w - 1, in_channel, index_batch] * weight[x_w, y_w, in_channel, out_channel]
                 end
                 output[x_out, y_out, out_channel, index_batch] = value
@@ -43,13 +48,16 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
 
         @tturbo for index_batch in 1:batch_size
             for out_channel in 1:out_channels, y_out in 1:output_height, x_out in 1:output_width
-                m = y_out + static((y_stride - 1)) * (y_out - 1)
-                n = x_out + static((x_stride - 1)) * (x_out - 1)
+                # m = y_out + static((y_stride - 1)) * (y_out - 1)
+                # n = x_out + static((x_stride - 1)) * (x_out - 1)
+                m = y_out + (y_stride - 1) * (y_out - 1)
+                n = x_out + (x_stride - 1) * (x_out - 1)
                 value = zero(T)
                 for in_channel in static(1):static(in_channels), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
                     # y_in = m + (y_w - 1)
                     # x_in = n + (x_w - 1)
-                    value += input[n + (x_w - 1), m + (y_w - 1), in_channel, index_batch] * weight[x_w, y_w, in_channel, out_channel]
+                    # value += input[n + (x_w - 1), m + (y_w - 1), in_channel, index_batch] * weight[x_w, y_w, in_channel, out_channel]
+                    value += input[n + x_w - 1, m + y_w - 1, in_channel, index_batch] * weight[x_w, y_w, in_channel, out_channel]
                 end
                 output[x_out, y_out, out_channel, index_batch] = value
             end
@@ -60,8 +68,10 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
 
         @tturbo for index_batch in 1:batch_size
             for out_channel in 1:out_channels, y_out in 1:output_height, x_out in 1:output_width
-                m = y_out + static((y_stride - 1)) * (y_out - 1)
-                n = x_out + static((x_stride - 1)) * (x_out - 1)
+                # m = y_out + static((y_stride - 1)) * (y_out - 1)
+                # n = x_out + static((x_stride - 1)) * (x_out - 1)
+                m = y_out + (y_stride - 1) * (y_out - 1)
+                n = x_out + (x_stride - 1) * (x_out - 1)
                 value = zero(T)
                 for in_channel in static(1):static(in_channels), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
                     # y_in = m + (y_w - 1) * y_dilation
@@ -77,8 +87,10 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
 
         @tturbo for index_batch in 1:batch_size
             for group in 1:groups, out_channel_per_group in 1:out_channels_per_group, y_out in 1:output_height, x_out in 1:output_width
-                m = y_out + static((y_stride - 1)) * (y_out - 1)
-                n = x_out + static((x_stride - 1)) * (x_out - 1)
+                # m = y_out + static((y_stride - 1)) * (y_out - 1)
+                # n = x_out + static((x_stride - 1)) * (x_out - 1)
+                m = y_out + (y_stride - 1) * (y_out - 1)
+                n = x_out + (x_stride - 1) * (x_out - 1)
                 out_channel = (group * out_channels_per_group + 1) - out_channel_per_group
                 value = zero(T)
                 for in_channel_weight in static(1):static(in_channels_weight), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
@@ -96,7 +108,8 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
     return output
 end
 
-#= # pad bounds check
+#=
+# pad bounds check
 function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, cdims::ConvDims; kw...) where {T<:Real}
 
     # fix for groupcount > 1 (NNlib.check_dims would throw an error otherwise)
@@ -146,8 +159,10 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
 
         @tturbo for index_batch in 1:batch_size
             for out_channel in 1:out_channels, y_out in h_region, x_out in w_region
-                m = y_out + static((y_stride - 1)) * (y_out - 1) - static(y_pad1)
-                n = x_out + static((x_stride - 1)) * (x_out - 1) - static(x_pad1)
+                # m = y_out + static((y_stride - 1)) * (y_out - 1) - static(y_pad1)
+                # n = x_out + static((x_stride - 1)) * (x_out - 1) - static(x_pad1)
+                m = y_out + (y_stride - 1) * (y_out - 1) - y_pad1
+                n = x_out + (x_stride - 1) * (x_out - 1) - x_pad1
                 value = zero(T)
                 for in_channel in static(1):static(in_channels), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
                     # y_in = m + (y_w - 1)
@@ -163,8 +178,10 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
 
         @tturbo for index_batch in 1:batch_size
             for out_channel in 1:out_channels, y_out in h_region, x_out in w_region
-                m = y_out + static((y_stride - 1)) * (y_out - 1) - static(y_pad1)
-                n = x_out + static((x_stride - 1)) * (x_out - 1) - static(x_pad1)
+                # m = y_out + static((y_stride - 1)) * (y_out - 1) - static(y_pad1)
+                # n = x_out + static((x_stride - 1)) * (x_out - 1) - static(x_pad1)
+                m = y_out + (y_stride - 1) * (y_out - 1) - y_pad1
+                n = x_out + (x_stride - 1) * (x_out - 1) - x_pad1
                 value = zero(T)
                 for in_channel in static(1):static(in_channels), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
                     # y_in = m + (y_w - 1) * y_dilation
@@ -180,8 +197,10 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
 
         @tturbo for index_batch in 1:batch_size
             for group in 1:groups, out_channel_per_group in 1:out_channels_per_group, y_out in h_region, x_out in w_region
-                m = y_out + static((y_stride - 1)) * (y_out - 1) - static(y_pad1)
-                n = x_out + static((x_stride - 1)) * (x_out - 1) - static(x_pad1)
+                # m = y_out + static((y_stride - 1)) * (y_out - 1) - static(y_pad1)
+                # n = x_out + static((x_stride - 1)) * (x_out - 1) - static(x_pad1)
+                m = y_out + (y_stride - 1) * (y_out - 1) - y_pad1
+                n = x_out + (x_stride - 1) * (x_out - 1) - x_pad1
                 out_channel = (group * out_channels_per_group + 1) - out_channel_per_group
                 value = zero(T)
                 for in_channel_weight in static(1):static(in_channels_weight), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
@@ -209,10 +228,13 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
                 
                     @tturbo for index_batch in 1:batch_size # @turbo
                         for out_channel in 1:out_channels, y_out in h_region, x_out in w_region
-                            m = y_out + static((y_stride - 1)) * (y_out - 1) - static(y_pad1)
-                            n = x_out + static((x_stride - 1)) * (x_out - 1) - static(x_pad1)
+                            # m = y_out + static((y_stride - 1)) * (y_out - 1) - static(y_pad1)
+                            # n = x_out + static((x_stride - 1)) * (x_out - 1) - static(x_pad1)
+                            m = y_out + (y_stride - 1) * (y_out - 1) - y_pad1
+                            n = x_out + (x_stride - 1) * (x_out - 1) - x_pad1
                             value = zero(T)
-                            for in_channel in static(1):static(in_channels), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
+                            # for in_channel in static(1):static(in_channels), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
+                            for in_channel in 1:in_channels, y_w in 1:weight_height, x_w in 1:weight_width
 
                                 y_in = m + (y_w - 1) * y_dilation
                                 x_in = n + (x_w - 1) * x_dilation
@@ -234,11 +256,14 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
 
                     @tturbo for index_batch in 1:batch_size # @turbo
                         for group in 1:groups, out_channel_per_group in 1:out_channels_per_group, y_out in h_region, x_out in w_region
-                            m = y_out + static((y_stride - 1)) * (y_out - 1) - static(y_pad1)
-                            n = x_out + static((x_stride - 1)) * (x_out - 1) - static(x_pad1)
+                            # m = y_out + static((y_stride - 1)) * (y_out - 1) - static(y_pad1)
+                            # n = x_out + static((x_stride - 1)) * (x_out - 1) - static(x_pad1)
+                            m = y_out + (y_stride - 1) * (y_out - 1) - y_pad1
+                            n = x_out + (x_stride - 1) * (x_out - 1) - x_pad1
                             out_channel = (group * out_channels_per_group + 1) - out_channel_per_group
                             value = zero(T)
-                            for in_channel_weight in static(1):static(in_channels_weight), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
+                            # for in_channel_weight in static(1):static(in_channels_weight), y_w in static(1):static(weight_height), x_w in static(1):static(weight_width)
+                            for in_channel in 1:in_channels, y_w in 1:weight_height, x_w in 1:weight_width
                                 
                                 y_in = m + (y_w - 1) * y_dilation
                                 x_in = n + (x_w - 1) * x_dilation
@@ -349,6 +374,18 @@ function NNlib.conv!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, 
 end
 =#
 
+function conv_im2col_grouped!(output::Array{T,4}, input::Array{T,4}, weight::Array{T,4}, cdims::ConvDims) where {T<:Real}
+
+    conv!(
+        NNlib.insert_singleton_spatial_dimension(output, 1),
+        NNlib.insert_singleton_spatial_dimension(input, 1),
+        NNlib.insert_singleton_spatial_dimension(weight, 1),
+        NNlib.insert_singleton_spatial_dimension(cdims, 1)
+    )
+
+    return output
+end
+
 function ∇conv_data_im2col_grouped!(input_gradient::Array{T,4}, output_gradient::Array{T,4}, weight::Array{T,4}, cdims::ConvDims) where {T<:Real}
 
     ∇conv_data!(
@@ -415,7 +452,8 @@ function NNlib.∇conv_data!(input_gradient::Array{T,4}, output_gradient::Array{
             for x_in in 0:input_width-1, y_in in 0:input_height-1, in_channel in 0:in_channels-1
 
                 value = zero(T)
-                for x_w in static(0):static(weight_width-1), y_w in static(0):static(weight_height-1), out_channel in static(0):static(out_channels-1)
+                # for x_w in static(0):static(weight_width-1), y_w in static(0):static(weight_height-1), out_channel in static(0):static(out_channels-1) # RICHTIG!!
+                for x_w in 0:weight_width-1, y_w in 0:weight_height-1, out_channel in 0:out_channels-1
 
                     is_in_bound_x = (x_in - x_w >= 0) & (x_in - x_w < output_width)
                     is_in_bound_y = (y_in - y_w >= 0) & (y_in - y_w < output_height)
@@ -442,3 +480,85 @@ function NNlib.∇conv_data!(input_gradient::Array{T,4}, output_gradient::Array{
 
     return input_gradient
 end
+
+#=
+function NNlib.∇conv_data!(input_gradient::Array{T,4}, output_gradient::Array{T,4}, weight::Array{T,4}, cdims::ConvDims; kw...) where {T<:Real}
+    # println("myconv data back called")
+    
+    size_weight_check_dims = (size(weight)[1:2]..., size(weight)[3]*cdims.groupcount, size(weight)[4])
+    cdims_check_dims = DenseConvDims(size(input_gradient), size_weight_check_dims, stride=cdims.stride, padding=cdims.padding, dilation=cdims.dilation, groups=1, flipkernel=cdims.flipkernel)
+    NNlib.check_dims(size(input_gradient), size_weight_check_dims, size(output_gradient), cdims_check_dims)
+    
+    # storing all the necessary shapes
+    output_width, output_height, out_channels, current_batch_size = size(output_gradient)
+    weight_width, weight_height, in_channels_weight, out_channels = size(weight)
+    # because in the actual computation section, values are added, it's saver to reset the given input_gradient first
+    input_gradient .= zero(T)
+    # check if input_gradient must be padded 
+    if cdims.padding != (0, 0, 0, 0)
+        input_gradient_padded = NNlib.pad_zeros(input_gradient, cdims.padding, dims=(1, 2))
+    else
+        input_gradient_padded = input_gradient
+    end
+    # store the size of input after padding 
+    input_width, input_height, in_channels, current_batch_size = size(input_gradient_padded) # size after padding
+
+    if !NNlib.flipkernel(cdims)
+        weight = reverse(weight, dims=(1, 2))
+    end
+
+    groups = cdims.groupcount
+    x_stride, y_stride = cdims.stride
+    x_dilation, y_dilation = cdims.dilation
+    out_channels_per_group = out_channels ÷ groups
+    # actual computation
+    if groups == 1 && cdims.stride == (1, 1) && cdims.dilation == (1, 1) # very specialized case for maximum performance
+        # println("very specialized case for maximum performance")
+        Threads.@threads for index_batch in 1:current_batch_size
+            @turbo for out_channel in 1:out_channels, y_out in 1:output_height, x_out in 1:output_width
+                for in_channel in 1:in_channels, y_w in 1:weight_height, x_w in 1:weight_width
+                    input_gradient_padded[x_out + x_w - 1, y_out + y_w - 1, in_channel, index_batch] += weight[x_w, y_w, in_channel, out_channel] * output_gradient[x_out, y_out, out_channel, index_batch]
+                end
+            end
+        end
+    elseif groups == 1 # second specialized case for better performance
+        # println("second specialized case for better performance")
+        Threads.@threads for index_batch in 1:current_batch_size
+            @turbo for out_channel in 1:out_channels, y_out in 1:output_height, x_out in 1:output_width
+                m = y_out + (y_stride - 1) * (y_out - 1)
+                n = x_out + (x_stride - 1) * (x_out - 1)
+                for in_channel in 1:in_channels, y_w in 1:weight_height, x_w in 1:weight_width
+                    y_in = m + (y_w - 1) * y_dilation
+                    x_in = n + (x_w - 1) * x_dilation
+                    input_gradient_padded[x_in, y_in, in_channel, index_batch] += weight[x_w, y_w, in_channel, out_channel] * output_gradient[x_out, y_out, out_channel, index_batch]
+                end
+            end
+        end
+    else # general case for any convolution 
+        # println("general case for any convolution")
+        Threads.@threads for index_batch in 1:current_batch_size
+            for out_channel_per_group in 1:out_channels_per_group # putting @turbo here may end up in wrong results
+                @turbo for group in 1:groups, y_out in 1:output_height, x_out in 1:output_width
+                    m = y_out + (y_stride - 1) * (y_out - 1)
+                    n = x_out + (x_stride - 1) * (x_out - 1)
+                    out_channel = (group * out_channels_per_group + 1) - out_channel_per_group
+                    for in_channel_weight in 1:in_channels_weight, y_w in 1:weight_height, x_w in 1:weight_width
+                        y_in = m + (y_w - 1) * y_dilation
+                        x_in = n + (x_w - 1) * x_dilation
+                        in_channel_input = in_channel_weight + (group - 1) * in_channels_weight
+                        input_gradient_padded[x_in, y_in, in_channel_input, index_batch] += weight[x_w, y_w, in_channel_weight, out_channel] * output_gradient[x_out, y_out, out_channel, index_batch]
+                    end
+                end
+            end
+        end
+    end
+
+    # depad 
+    if cdims.padding != (0, 0, 0, 0)
+        x_pad1, x_pad2, y_pad1, y_pad2 = cdims.padding
+        input_gradient .= input_gradient_padded[x_pad1+1:input_width-x_pad2, y_pad1+1:input_height-y_pad2, :, :]
+    end
+
+    return input_gradient
+end
+=#
