35f2b772944ba594aa93483691501c79cf65808c
Jonas S <74866941+jonas208@users.noreply.github.com>
2023-10-08 19:36:53 +0200

    Add files via upload

--- test/ext_loopvectorization/runtests.jl
+++ test/ext_loopvectorization/runtests.jl
@@ -32,7 +32,7 @@ function compute_pool_outputs(settings::Vector{<:NNlib.PoolDims}, input::Array{T
     return pool_outs
 end
 
-@testset "Convolution & Pooling" begin
+@testset "Convolution & Pooling & Activations" begin
     
     dtype = Float32 # Float64
     batch_size = 64 # 1 # 64 # 32
@@ -44,34 +44,44 @@ end
         NNlib.DenseConvDims(size(input), size(weight_ungrouped), stride=(1, 1), padding=(0, 0), dilation=(1, 1), groups=1), # test 'very specialized case'
         NNlib.DenseConvDims(size(input), size(weight_ungrouped), stride=(2, 1), padding=(0, 0), dilation=(1, 1), groups=1), # test 'second specialized case'
         NNlib.DenseConvDims(size(input), size(weight_ungrouped), stride=(2, 1), padding=(0, 0), dilation=(2, 1), groups=1), # test 'third specialized case'
-        ### NNlib.DenseConvDims(size(input), size(weight_grouped), stride=(2, 1), padding=(2, 0), dilation=(2, 1), groups=3), # test 'general case'
-        NNlib.DenseConvDims(size(input), size(weight_grouped), stride=(2, 1), padding=(2, 1), dilation=(2, 1), groups=3), # test 'general case'
+        NNlib.DenseConvDims(size(input), size(weight_grouped), stride=(2, 1), padding=(2, 0), dilation=(2, 1), groups=3), # test 'general case'
     ]
 
     conv_output_grads = [rand(dtype, NNlib.output_size(setting)..., 27, batch_size) for setting in conv_settings_list]
 
     pool_settings_list = [
         NNlib.PoolDims(size(input), (5, 4), stride=(1, 1), padding=(0, 0), dilation=(1, 1)), # test 'specialized case'
-        # NNlib.PoolDims(size(input), (5, 4), stride=(5, 4), padding=(2, 0), dilation=(2, 1)), # test 'general case'
-        NNlib.PoolDims(size(input), (5, 4), stride=(5, 4), padding=(2, 1), dilation=(2, 1)), # test 'general case'
+        NNlib.PoolDims(size(input), (5, 4), stride=(5, 4), padding=(2, 0), dilation=(2, 1)), # test 'general case'
     ]
+
+    modified_activations = [tanh, softsign, softplus, sigmoid, sigmoid_fast, hardsigmoid, logsigmoid, swish]
 
     # compute outputs before loading LoopVectorization
     
     println("without LoopVectorization")
     conv_outs_std, conv_grads_std = compute_conv_outputs(conv_settings_list, input, weight_ungrouped, weight_grouped, conv_output_grads)
     pool_outs_std = compute_pool_outputs(pool_settings_list, input)
+    act_outs_std = [@btime $act.($input) for act in modified_activations]
 
     using LoopVectorization # now load the NNlibLoopVectorizationExt
 
     println("with LoopVectorization")
     conv_outs_lv, conv_grads_lv = compute_conv_outputs(conv_settings_list, input, weight_ungrouped, weight_grouped, conv_output_grads)
     pool_outs_lv = compute_pool_outputs(pool_settings_list, input)
+    act_outs_lv = [@btime $act.($input) for act in modified_activations]
 
     # validate conv
-    @test all(isapprox.(conv_outs_std, conv_outs_lv))
-    @test all(isapprox.(conv_grads_std, conv_grads_lv))
+    @testset "Convolution" begin
+        @test all(isapprox.(conv_outs_std, conv_outs_lv))
+        @test all(isapprox.(conv_grads_std, conv_grads_lv))
+    end
     # validate pool
-    @test all(isapprox.(pool_outs_std, pool_outs_lv))
+    @testset "Pooling" begin
+        @test all(isapprox.(pool_outs_std, pool_outs_lv))
+    end
+    # validate activations
+    @testset "Activations" begin
+        @test all(isapprox.(act_outs_std, act_outs_lv))
+    end
 
 end
