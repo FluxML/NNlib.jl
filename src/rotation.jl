5a2ea46b4b6dc936d0e814b2e2263d04455d6997
roflmaostc <fxw+git@mailbox.org>
2023-12-17 18:39:54 +0100

    clear docs

--- src/rotation.jl
+++ src/rotation.jl
@@ -1,3 +1,5 @@
+export imrotate
+
 
 # this rotates the coordinates and either applies round(nearest neighbour)
 # or floor (:bilinear interpolation)
@@ -34,13 +36,12 @@ end
 
 
 """
-    imrotate(arr::AbstractArray, θ; method=:bilinear, midpoint=size(arr) .÷ 2 .+ 1)
+    imrotate(arr::AbstractArray{T, 4}, θ; method=:bilinear, midpoint=size(arr) .÷ 2 .+ 1)
 
 Rotates a matrix around the center pixel `midpoint`.
 The angle `θ` is interpreted in radians.
 
 The adjoint is defined with ChainRulesCore.jl. This method also runs with CUDA (and in principle all KernelAbstractions.jl supported backends).
-If `arr` is a `AbstractArray{T, 3}`, the third dimension is interpreted as batch dimension.
 
 # Keywords
 * `method=:bilinear` for bilinear interpolation or `method=:nearest` for nearest neighbour
@@ -48,65 +49,6 @@ If `arr` is a `AbstractArray{T, 3}`, the third dimension is interpreted as batch
 
 # Examples
 ```julia-repl
-julia> arr = zeros((6, 6)); arr[3:4, 4] .= 1;
-
-julia> arr
-6×6 Matrix{Float64}:
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  0.0  0.0  1.0  0.0  0.0
- 0.0  0.0  0.0  1.0  0.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
-
-julia> imrotate(arr, deg2rad(45))
-6×6 view(::Array{Float64, 3}, :, :, 1) with eltype Float64:
- 0.0  0.0  0.0        0.0        0.0       0.0
- 0.0  0.0  0.0        0.0        0.0       0.0
- 0.0  0.0  0.0        0.292893   0.585786  0.0
- 0.0  0.0  0.0857864  1.0        0.292893  0.0
- 0.0  0.0  0.0        0.0857864  0.0       0.0
- 0.0  0.0  0.0        0.0        0.0       0.0
-
-julia> imrotate(arr, deg2rad(90))
-6×6 view(::Array{Float64, 3}, :, :, 1) with eltype Float64:
- 0.0  0.0  0.0  0.0  0.0          0.0
- 0.0  0.0  0.0  0.0  0.0          0.0
- 0.0  0.0  0.0  0.0  1.11022e-16  0.0
- 0.0  0.0  0.0  1.0  1.0          0.0
- 0.0  0.0  0.0  0.0  0.0          0.0
- 0.0  0.0  0.0  0.0  0.0          0.0
-
-julia> imrotate(arr, deg2rad(90), midpoint=(3,3))
-6×6 view(::Array{Float64, 3}, :, :, 1) with eltype Float64:
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  1.0  1.0  0.0  0.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
-
-julia> imrotate(arr, deg2rad(90), method=:nearest)
-6×6 view(::Array{Float64, 3}, :, :, 1) with eltype Float64:
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  0.0  0.0  1.0  1.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
- 0.0  0.0  0.0  0.0  0.0  0.0
-
-julia> using CUDA
-
-julia> Array(imrotate(CuArray(arr), deg2rad(90))) ≈ imrotate(arr, deg2rad(90))
-true
-
-julia> using Zygote
-
-julia> f(x) = sum(abs2.(imrotate(x, π/4)))
-f (generic function with 1 method)
-
-julia> Zygote.gradient(f, arr)
-([0.0 0.0 … 0.0 0.0; 0.0 0.0 … 5.387705551013979e-17 0.0; … ; 0.0 0.0 … 0.08578643762690495 0.0; 0.0 0.0 … 0.0 0.0],)
 
 ```
 """
