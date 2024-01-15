8923d494355d30e166cb63ea86154f5e995c4268
roflmaostc <fxw+git@mailbox.org>
2024-01-15 23:23:22 +0100

    Refine rotation tests

--- test/rotation.jl
+++ test/rotation.jl
@@ -4,87 +4,75 @@ function rotation_testsuite(Backend)
     T = Float32
     atol = T == Float32 ? 1e-3 : 1e-6
     rtol = T == Float32 ? 1f-2 : 1f-6
-    
-    @testset "Image Rotation" begin
+    angles = deg2rad.([0, 0.0, 0.0001, 35, 90, -90, -90.0123, 170, 180, 270, 360, 450, 1234.1234]) 
+
+    @testset "imrotate" begin
         @testset "Simple test" begin
             arr = device(zeros((6, 6, 1, 1))); 
             arr[3:4, 4, 1, 1] .= 1;
             @test all(cpu(NNlib.imrotate(arr, deg2rad(45))) .≈ [0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.29289321881345254 0.585786437626905 0.0; 0.0 0.0 0.08578643762690495 1.0 0.2928932188134524 0.0; 0.0 0.0 0.0 0.08578643762690495 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0])
         end
-    end
 
 
-    @testset "Compare with ImageTransformations" begin
-        arr = device(zeros(T, (51, 51, 1, 1)))
-        arr[15:40, 15:40, :, :] .= device(1 .+ randn((26, 26)))                                                                       
-        
-        arr2 = device(zeros(T, (51, 51, 1, 5)))
-        arr2[15:40, 15:40, :, :] .= device(arr[15:40, 15:40, :, :])
+        @testset "Compare with ImageTransformations" begin
+            for sz in [(51,51,1,1)]
+                
+                arr1 = device(zeros(T, sz))
+                arr1[15:40, 15:40, :, :] .= device(1 .+ randn((26, 26)))                                                                       
+                arr2 = device(zeros(T, (sz[1], sz[2], sz[3], 3)))
+                arr2[15:40, 15:40, :, :] .= device(arr1[15:40, 15:40, :, :])
 
-
-        for method in [:nearest, :bilinear]
-            for angle in deg2rad.([0, 35, 45, 90, 135, 170, 180, 270, 360])
-                res1 = cpu(NNlib.imrotate(arr, angle; method))
-                res3 = cpu(NNlib.imrotate(arr2, angle; method))
-                if method == :nearest
-                    res2 = ImageTransformations.imrotate(cpu(arr)[:, :, 1, 1], angle, axes(arr)[1:2], method=Constant(), fillvalue=0)
-                elseif method == :bilinear
-                    res2 = ImageTransformations.imrotate(cpu(arr)[:, :, 1, 1], angle, axes(arr)[1:2], fillvalue=0)
+                for method in [:nearest, :bilinear]
+                    @testset "$method" begin
+                        for angle in angles
+                            if iseven(sz[1])
+                                res1 = cpu(NNlib.imrotate(arr1, angle; method, midpoint=size(arr1).÷2 .+ 0.5))
+                                res2 = cpu(NNlib.imrotate(arr2, angle; method, midpoint=size(arr1).÷2 .+ 0.5))
+                            else
+                                res1 = cpu(NNlib.imrotate(arr1, angle; method))
+                                res2 = cpu(NNlib.imrotate(arr2, angle; method))
+                            end
+                            if method == :nearest
+                                res_IT = ImageTransformations.imrotate(cpu(arr1)[:, :, 1, 1], angle, axes(arr1)[1:2], method=Constant(), fillvalue=0)
+                            elseif method == :bilinear
+                                res_IT = ImageTransformations.imrotate(cpu(arr1)[:, :, 1, 1], angle, axes(arr1)[1:2], fillvalue=0)
+                            end
+                            @test all(.≈(1 .+ res1[:, :, :, :], 1 .+ res_IT[:, :], rtol=rtol))
+                            @test all(.≈(1 .+ res1[:, :, :, :], 1 .+ res2[:, :,:, 1], rtol=rtol))
+                            @test all(.≈(1 .+ res1[:, :, :, :], 1 .+ res2[:, :,:, 2], rtol=rtol))
+                        end
+                    end
                 end
-                @test all(.≈(1 .+ res1[:, :, :, :], 1 .+ res2[:, :], rtol=rtol))
-                @test all(.≈(1 .+ res1[:, :, :, :], 1 .+ res3[:, :,:, 1], rtol=rtol))
-                @test all(.≈(1 .+ res1[:, :, :, :], 1 .+ res3[:, :,:, 2], rtol=rtol))
             end
         end
-        
-        arr = device(zeros(T, (52, 52, 1, 1)))
-        arr[15:40, 15:40, :, :] .= device(1 .+ randn((26, 26)))
-        
-        arr2 = device(zeros(T, (52, 52, 5, 1)))
-        arr2[15:40, 15:40, :, 1] .= device(arr[15:40, 15:40, :, 1])
+            
+        @testset "Compare for plausibilty" begin
+            @testset "Special cases of rotation" begin
+                arr = device(zeros(T, (10, 10, 1, 3)))
+                arr[6, 6, :, 1] .= 1
+                arr[6, 6, :, 2] .= 2
+                arr[6, 6, :, 3] .= 2
 
-        for method in [:nearest, :bilinear]
-            for angle in deg2rad.([0, 35,  90, 170, 180, 270, 360])
-                res1 = cpu(NNlib.imrotate(arr, angle; method, midpoint=size(arr) .÷2 .+0.5))
-                res3 = cpu(NNlib.imrotate(arr2, angle; method, midpoint=size(arr) .÷2 .+0.5))
-                if method == :nearest
-                    res2 = ImageTransformations.imrotate(cpu(arr)[:, :, 1, 1], angle, axes(arr)[1:2], method=Constant(), fillvalue=0)
-                elseif method == :bilinear
-                    res2 = ImageTransformations.imrotate(cpu(arr)[:, :, 1, 1], angle, axes(arr)[1:2], fillvalue=0)
+                for method in [:bilinear, :nearest]
+                    @test all(.≈(arr , NNlib.imrotate(arr, deg2rad(0); method)))
+                    @test all(.≈(arr , NNlib.imrotate(arr, deg2rad(90); method)))
+                    @test all(.≈(arr , NNlib.imrotate(arr, deg2rad(180); method)))
+                    @test all(.≈(arr , NNlib.imrotate(arr, deg2rad(270); method)))
+                    @test all(.≈(arr , NNlib.imrotate(arr, deg2rad(360); method)))
                 end
-                @test all(1 .+ res1[:, :, :, :] .≈ 1 .+ res2[:, :])
-                @test all(1 .+ res1[:, :, :, :] .≈ 1 .+ res3[:, :, :, :])
-                @test all(1 .+ res1[:, :, :, :] .≈ 1 .+ res3[:, :, :, :])
             end
         end
 
-    end
-
-    @testset "Compare for plausibilty" begin
-        arr = zeros(T, (10, 10, 1, 3))
-        arr[6, 6, :, 1] .= 1
-        arr[6, 6, :, 2] .= 2
-        arr[6, 6, :, 3] .= 2
-
-        for method in [:bilinear, :nearest]
-            @test all(.≈(arr , NNlib.imrotate(arr, deg2rad(0); method)))
-            @test all(.≈(arr , NNlib.imrotate(arr, deg2rad(90); method)))
-            @test all(.≈(arr , NNlib.imrotate(arr, deg2rad(180); method)))
-            @test all(.≈(arr , NNlib.imrotate(arr, deg2rad(270); method)))
-            @test all(.≈(arr , NNlib.imrotate(arr, deg2rad(360); method)))
-        end
-    end
-
-
-    @testset "Test gradients" begin
-        for method in [:nearest, :bilinear]
-            for angle in deg2rad.([0, 0.0, 0.0001, 35, 90, -90, -90.0123, 170, 180, 270, 360, 450, 1234.1234])
-                gradtest_fn(
-                    x -> NNlib.imrotate(x, angle; method),
-                    device(rand(T, 11,11,1,1)); atol)
-                gradtest_fn(
-                    x -> NNlib.imrotate(x, angle; method),
-                    device(rand(T, 10,10,1,1)); atol)        
+        @testset "Test gradients" begin
+            for method in [:nearest, :bilinear]
+                for angle in angles 
+                    gradtest_fn(
+                        x -> NNlib.imrotate(x, angle; method),
+                        device(rand(T, 11,11,1,1)); atol)
+                    gradtest_fn(
+                        x -> NNlib.imrotate(x, angle; method),
+                        device(rand(T, 10,10,1,1)); atol)        
+                end
             end
         end
     end
