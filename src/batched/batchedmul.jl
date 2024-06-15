627374ca51090c4e5b9cfbed5c15bdaa992891f8
Lance (Weiqing) Xu <47257262+lanceXwq@users.noreply.github.com>
2024-06-15 06:10:04 -0700

    fix typos in the doc for batched_mul! (#590)

--- src/batched/batchedmul.jl
+++ src/batched/batchedmul.jl
@@ -203,7 +203,7 @@ In-place batched matrix multiplication, equivalent to
 If `size(B,3) == 1` then every batch uses `B[:,:,1]` instead.
 
 This will call `batched_gemm!` whenever possible. For real arrays this means that,
-for `X ∈ [A,B,C]`, either `strides(X,1)==1` or `strides(X,2)==1`, the latter may
+for `X ∈ [A,B,C]`, either `stride(X,1)==1` or `stride(X,2)==1`, the latter may
 be caused by `batched_transpose` or by for instance `PermutedDimsArray(::Array, (3,1,2))`.
 Unlike `batched_mul` this will never make a copy.
 
