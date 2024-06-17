0e318fd4fa305c36655615c362667c9a044f31a3
adrhill <adrian.hill@mailbox.org>
2024-06-17 22:49:05 +0200

    Add fix to `conv_direct!`

--- src/impl/conv_direct.jl
+++ src/impl/conv_direct.jl
@@ -81,6 +81,9 @@ function conv_direct!(
     # Use `calc_padding_regions` to determine where we do or don't need to worry about padding
     padded_regions, central_region = calc_padding_regions(cdims)
 
+    # Set outputs to zero (https://github.com/FluxML/NNlib.jl/issues/490)
+    y = fill!(y, zero(yT))
+
     # Start with the central region
     w_region, h_region, d_region = central_region
     @inbounds for batch in 1:size(x, 5),
