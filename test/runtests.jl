361ecbe9cb7f23121bd58ce2eecd3680d39a16e1
Carlo Lucibello <carlo.lucibello@gmail.com>
2026-06-30 08:19:44 +0200

    or

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,10 +16,12 @@ const NNLIB_TEST_AMDGPU   = get(ENV, "NNLIB_TEST_AMDGPU",   "false") == "true"
 const NNLIB_TEST_METAL    = get(ENV, "NNLIB_TEST_METAL",    "false") == "true"
 const NNLIB_TEST_THREADED = get(ENV, "NNLIB_TEST_THREADED", "false") == "true"
 
-const NNLIB_TEST_ENZYME =   get(ENV, "NNLIB_TEST_ENZYME",   "true")  == "true" &&
-                            VERSION <= v"1.13-" && # fails on nighlt
-                            !NNLIB_TEST_AMDGPU && !NNLIB_TEST_METAL && !NNLIB_TEST_CUDA && # TODO fails on GPU backends
-                            !Sys.iswindows() # TODO fails on Windows
+const NNLIB_TEST_ENZYME =   get(ENV, "NNLIB_TEST_ENZYME",   "true")  == "true" ||
+                            (
+                                VERSION <= v"1.13-" && # fails on nightly
+                                !NNLIB_TEST_AMDGPU && !NNLIB_TEST_METAL && !NNLIB_TEST_CUDA && # TODO fails on GPU backends
+                                !Sys.iswindows() # TODO fails on Windows
+                            )
 
 # Tests that exercise NNlib's multithreaded code paths (`@spawn` / `@threads`).
 # The dedicated `NNLIB_TEST_THREADED` job runs *only* these, on multithreaded
