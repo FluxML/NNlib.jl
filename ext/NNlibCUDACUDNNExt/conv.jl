fa0815875127cf02dda2da2274091022a87de655
Tim Besard <tim.besard@gmail.com>
2026-07-10 08:55:23 +0200

    NNlibCUDA: name complex convolution helper directly

--- ext/NNlibCUDACUDNNExt/conv.jl
+++ ext/NNlibCUDACUDNNExt/conv.jl
@@ -25,7 +25,10 @@ conv_kwargs(cdims::DenseConvDims, ::Type{T}) where {T} =
 conv_bias_activation(::typeof(NNlib.relu)) = :relu
 conv_bias_activation(::Any) = nothing
 
-@inline function _complex!(y::DenseCuArray{T1}, yr::DenseCuArray{T2}, yi::DenseCuArray{T2}; bias=zero(T1), alpha=one(T1), beta=zero(T1), σ=identity) where {T1 <: CUDNNComplexFloat, T2<:CUDNNFloat}
+@inline function combine_complex!(y::DenseCuArray{T1}, yr::DenseCuArray{T2},
+                                  yi::DenseCuArray{T2}; bias=zero(T1), alpha=one(T1),
+                                  beta=zero(T1), σ=identity,
+) where {T1<:CUDNNComplexFloat,T2<:CUDNNFloat}
     # if y is from similar(), it may have NaNs, and beta*NaN will propagate.
     if beta != 0
         @. y = σ(alpha*(yr + im*yi) + bias + beta*y)
@@ -59,7 +62,7 @@ function conv!(y::DenseCuArray{T}, x::DenseCuArray{T}, w::DenseCuArray{T}, cdims
     a = conv!(similar(real(y)), xr, wr, cdims; algo=algo)
     b = conv!(similar(a), xi, wi, cdims; algo=algo)
     c = conv!(similar(a), xr + xi, wr + wi, cdims; algo=algo)
-    return _complex!(y, a - b, c - a - b; alpha=alpha, beta=beta)
+    return combine_complex!(y, a - b, c - a - b; alpha, beta)
 end
 
 # (xr + im*xi) * w = xr*w + im*(xi*w)
@@ -68,7 +71,7 @@ function conv!(y::DenseCuArray{T1}, x::DenseCuArray{T1}, w::DenseCuArray{T2}, cd
     xr, xi = reim(x)
     yr = conv!(similar(real(y)), xr, w, cdims; algo=algo)
     yi = conv!(similar(yr), xi, w, cdims; algo=algo)
-    return _complex!(y, yr, yi; alpha=alpha, beta=beta)
+    return combine_complex!(y, yr, yi; alpha, beta)
 end
 
 # x * (wr + im*wi) = x*wr + im*(x*wi)
@@ -77,7 +80,7 @@ function conv!(y::DenseCuArray{T1}, x::DenseCuArray{T2}, w::DenseCuArray{T1}, cd
     wr, wi = reim(w)
     yr = conv!(similar(real(y)), x, wr, cdims; algo=algo)
     yi = conv!(similar(yr), x, wi, cdims; algo=algo)
-    return _complex!(y, yr, yi; alpha=alpha, beta=beta)
+    return combine_complex!(y, yr, yi; alpha, beta)
 end
 
 function conv_bias_act!(y::DenseCuArray{T}, x::DenseCuArray{T}, w::DenseCuArray{T},
@@ -104,7 +107,7 @@ function conv_bias_act!(y::DenseCuArray{T}, x::DenseCuArray{T}, w::DenseCuArray{
     a = conv!(similar(real(y)), xr, wr, cdims; alpha=1, beta=0, algo=algo)
     b = conv!(similar(a), xi, wi, cdims; alpha=1, beta=0, algo=algo)
     c = conv!(similar(a), xr + xi, wr + wi, cdims; alpha=1, beta=0, algo=algo)
-    return _complex!(y, a - b, c - a - b; bias=bias, alpha=alpha, beta=beta, σ=σ)
+    return combine_complex!(y, a - b, c - a - b; bias, alpha, beta, σ)
 end
 
 function ∇conv_data!(dx::DenseCuArray{T}, dy::DenseCuArray{T}, w::DenseCuArray{T},
@@ -123,7 +126,7 @@ function ∇conv_data!(dx::DenseCuArray{T}, dy::DenseCuArray{T}, w::DenseCuArray
     a = ∇conv_data!(similar(real(dx)), dyr, wr, cdims; alpha=1, beta=0, algo=algo)
     b = ∇conv_data!(similar(a), dyi, -wi, cdims; alpha=1, beta=0, algo=algo)
     c = ∇conv_data!(similar(a), dyr + dyi, wr - wi, cdims; alpha=1, beta=0, algo=algo)
-    return _complex!(dx, a - b, c - a - b; alpha=alpha, beta=beta)
+    return combine_complex!(dx, a - b, c - a - b; alpha, beta)
 end
 
 # dx = (dyr + im*dyi)*w = dyr*w + im*(dyi*w)
@@ -132,7 +135,7 @@ function ∇conv_data!(dx::DenseCuArray{T1}, dy::DenseCuArray{T1}, w::DenseCuArr
     dyr, dyi = reim(dy)
     dxr = ∇conv_data!(similar(real(dx)), dyr, w, cdims; alpha=1, beta=0, algo=algo)
     dxi = ∇conv_data!(similar(dxr), dyi, w, cdims; alpha=1, beta=0, algo=algo)
-    return _complex!(dx, dxr, dxi; alpha=alpha, beta=beta)
+    return combine_complex!(dx, dxr, dxi; alpha, beta)
 end
 
 function ∇conv_filter!(dw::DenseCuArray{T}, x::DenseCuArray{T}, dy::DenseCuArray{T},
@@ -151,7 +154,7 @@ function ∇conv_filter!(dw::DenseCuArray{T}, x::DenseCuArray{T}, dy::DenseCuArr
     a = ∇conv_filter!(similar(real(dw)), xr, dyr, cdims; alpha=1, beta=0, algo=algo)
     b = ∇conv_filter!(similar(a), -xi, dyi, cdims; alpha=1, beta=0, algo=algo)
     c = ∇conv_filter!(similar(a), xr - xi, dyr + dyi, cdims; alpha=1, beta=0, algo=algo)
-    return _complex!(dw, a - b, c - a - b; alpha=alpha, beta=beta)
+    return combine_complex!(dw, a - b, c - a - b; alpha, beta)
 end
 
 # dw = x*(dyr + im*dyi) = x*dyr + im*(x*dyi)
@@ -160,5 +163,5 @@ function ∇conv_filter!(dw::DenseCuArray{T1}, x::DenseCuArray{T2}, dy::DenseCuA
     dyr, dyi = reim(dy)
     dwr = ∇conv_filter!(similar(real(dw)), x, dyr, cdims; alpha=1, beta=0, algo=algo)
     dwi = ∇conv_filter!(similar(dwr), x, dyi, cdims; alpha=1, beta=0, algo=algo)
-    return _complex!(dw, dwr, dwi; alpha=alpha, beta=beta)
+    return combine_complex!(dw, dwr, dwi; alpha, beta)
 end
